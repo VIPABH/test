84f26ae767430ea4db994ef43fa30cc4bccf0dc9
VIPABH <a891983.2@gmail.com>
2025-03-15 02:25:49 +0300

    Update p.py

--- p.py
+++ p.py
@@ -30,15 +30,16 @@ async def show_res(event):
         await event.reply(f"المستخدم [{unm}](tg://user?id={uid}) أرسل {msg_count} رسالة في هذه المجموعة.")
 @ABH.on(events.NewMessage(pattern='رسائله|رسائلة|رسائل|الرسائل'))
 async def show_res(event):
+
     await asyncio.sleep(2)
-    if not uid or not uid:
+    if not uid1 or not uid1:
         return
     r = await event.get_reply_message()
-    uid = r.sender.first_name
-    unm = r.sender_id
-    guid = event.chat_id
-    if unm in uinfo and guid in uinfo[unm]:
-        msg_count = uinfo[unm][guid]["msg"]
-        await event.reply(f"المستخدم [{unm}](tg://user?id={uid}) أرسل {msg_count} رسالة في هذه المجموعة.")
+    uid1 = r.sender.first_name
+    unm1 = r.sender_id
+    guid1 = event.chat_id
+    if unm1 in uinfo and guid1 in uinfo[unm1]:
+        msg_count = uinfo[unm1][guid1]["msg"]
+        await event.reply(f"المستخدم [{unm1}](tg://user?id={uid1}) أرسل {msg_count} رسالة في هذه المجموعة.")
 
 ABH.run_until_disconnected()
