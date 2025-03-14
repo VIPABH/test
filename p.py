70cf507ac08659b2d3522286a749fe6a24d06bac
VIPABH <a891983.2@gmail.com>
2025-03-15 02:50:42 +0300

    Update p.py

--- p.py
+++ p.py
@@ -5,7 +5,9 @@ api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 ABH = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
+
 uinfo = {}
+
 @ABH.on(events.NewMessage)
 async def msgs(event):
     global uinfo
@@ -19,20 +21,26 @@ async def msgs(event):
             uinfo[unm][guid] = {"guid": guid, "unm": unm, "fname": uid, "msg": 1}
         else:
             uinfo[unm][guid]["msg"] += 1
+
 @ABH.on(events.NewMessage(pattern='توب'))
 async def show_res(event):
     await asyncio.sleep(2)
     guid = event.chat_id
-    unm = event.sender_id
-    sorted_users = sorted(uinfo.items(), key=lambda x: x[1][guid]['msg'], reverse=True)[:15]
+    
+    sorted_users = sorted(uinfo.items(), key=lambda x: x[1][guid]['msg'], reverse=True)[:20]
+    
     top_users = []
     for user, data in sorted_users:
         if guid in data:
-            top_users.append(f"{data[guid][unm]['msg']} رسائل")
+            user_id = user  # استخدم المعرف الخاص بالمستخدم
+            msg_count = data[guid]["msg"]
+            top_users.append(f"المستخدم [{data[guid]['fname']}](tg://user?id={user_id}) رسائله -> {msg_count}")
+    
     if top_users:
         await event.reply("\n".join(top_users))
     else:
         await event.reply("لا توجد بيانات لعرضها.")
+
 @ABH.on(events.NewMessage(pattern='رسائله|رسائلة|رسائل|الرسائل'))
 async def show_res(event):
     r = await event.get_reply_message()
@@ -45,4 +53,5 @@ async def show_res(event):
     if unm1 in uinfo and guid1 in uinfo[unm1]:
         msg_count = uinfo[unm1][guid1]["msg"]
         await event.reply(f"المستخدم [{uid1}](tg://user?id={unm1}) أرسل {msg_count} رسالة في هذه المجموعة.")
+
 ABH.run_until_disconnected()
