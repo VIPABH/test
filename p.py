2d08a0d0f8775d0b41907a0644a4f94bdddab6b2
VIPABH <a891983.2@gmail.com>
2025-03-15 03:20:44 +0300

    Update p.py

--- p.py
+++ p.py
@@ -16,7 +16,7 @@ async def msgs(event):
     formatted_time = time.strftime("%H:%M", now)
 
     # إذا كان الوقت 15:15، مسح البيانات
-    if formatted_time == "03:19":
+    if formatted_time == "03:21":
         uinfo = {}
         print("تم مسح البيانات عند الساعة 15:15.")
     
