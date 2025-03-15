5e39555c5f890ad9e6fcda2e1521405b33a8398c
VIPABH <a891983.2@gmail.com>
2025-03-15 14:16:34 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
-import os, asyncio, time
-
+from datetime import datetime
+import os, asyncio
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
@@ -13,22 +13,23 @@ async def msgs(event):
         uid = event.sender.first_name
         unm = event.sender_id
         guid = event.chat_id
-        if unm not in uinfo:
-            uinfo[unm] = {}
-        if guid not in uinfo[unm]:
-            uinfo[unm][guid] = {"guid": guid, "unm": unm, "fname": uid, "msg": 1}
-        else:
-            uinfo[unm][guid]["msg"] += 1
+        uinfo.setdefault(unm, {}).setdefault(guid, {"guid": guid, "unm": unm, "fname": uid, "msg": 0})["msg"] += 1
+    if timenow == targetdate:
+        now = datetime.now()
+        timenow = now.strftime("%I:%M %p")
+        targetdate = "02:17 PM"
+        uinfo.setdefault(unm, {}).setdefault(guid, {"guid": guid, "unm": unm, "fname": uid, "msg": 0})
 @ABH.on(events.NewMessage(pattern='توب'))
 async def show_res(event):
     await asyncio.sleep(2)
     guid = event.chat_id
-    unm = event.sender_id
-    sorted_users = sorted(uinfo.items(), key=lambda x: x[1][guid]['msg'], reverse=True)[:15]
+    sorted_users = sorted(uinfo.items(), key=lambda x: x[1][guid]['msg'], reverse=True)[:20]
     top_users = []
     for user, data in sorted_users:
         if guid in data:
-            top_users.append(f"{data[guid][unm]['msg']} رسائل")
+            user_id = user 
+            msg_count = data[guid]["msg"]
+            top_users.append(f"المستخدم [{data[guid]['fname']}](tg://user?id={user_id}) رسائله -> {msg_count}")
     if top_users:
         await event.reply("\n".join(top_users))
     else:
@@ -41,10 +42,10 @@ async def show_res(event):
     guid1 = event.chat_id
     if unm1 in uinfo and guid1 in uinfo[unm1]:
         msg_count = uinfo[unm1][guid1]["msg"]
-        await event.reply(f"المستخدم [{uid1}](tg://user?id={unm1}) ارسلت {msg_count} رسالة في هذه المجموعة.")
+        await event.reply(f"المستخدم [{uid1}](tg://user?id={unm1}) أرسلت {msg_count} رسالة في هذه المجموعة.")
 @ABH.on(events.NewMessage(pattern='رسائله|رسائلة|رسائل|الرسائل'))
 async def show_res(event):
-    r = await event.get_reply_message()
+    r = await event.get_reply_message()  
     await asyncio.sleep(2)
     if not r:
         return
@@ -54,4 +55,7 @@ async def show_res(event):
     if unm1 in uinfo and guid1 in uinfo[unm1]:
         msg_count = uinfo[unm1][guid1]["msg"]
         await event.reply(f"المستخدم [{uid1}](tg://user?id={unm1}) أرسل {msg_count} رسالة في هذه المجموعة.")
+@ABH.on(events.NewMessage(pattern='/الرسائل'))
+async def title(event):
+    await event.reply('اهلا صديقي , اوامر الرسائل \n ارسل `توب` ل اضهار توب 15 تفاعل \n ارسل `رسائلي` ل اضهار رسائلك في اخر يوم \n ارسل `رسائله` ل اضهار رساله الشخص بالرد \n استمتع')
 ABH.run_until_disconnected()
