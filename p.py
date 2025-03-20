05b56e85e29a57a7dbb5731adae9a6bd24a20b93
VIPABH <a891983.2@gmail.com>
2025-03-21 00:36:23 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,12 +4,13 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-points = {'uid': uid, "nid": nid, "gid": gid, "points": 0}
+points = {}
 @ABH.on(events.NewMessage)
 async def p(event):
     global uid, nid , gid 
     uid = event.sender_id
     nid = event.sender.username
     gid = event.chat_id
-    points["points"] += 1
+    points = {'uid': uid, "nid": nid, "gid": gid, "points": 0}
+    points["points"] += 2
 ABH.run_until_disconnected()
