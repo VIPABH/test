90a5072668b5f27c01d7764b172339da5326f60c
VIPABH <a891983.2@gmail.com>
2025-03-27 03:35:23 +0300

    Update p.py

--- p.py
+++ p.py
@@ -15,36 +15,36 @@ bot_token = os.getenv('BOT_TOKEN')
 # إعدادات العميل
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-# دالة لتحميل الفيديو باستخدام yt-dlp
-async def download_video(url: str):
+# دالة لتحميل الصوت باستخدام yt-dlp
+async def download_audio(url: str):
     ydl_opts = {
-        'outtmpl': '-',  # تحميل الفيديو مباشرة إلى الذاكرة
+        'outtmpl': '-',  # تحميل الصوت مباشرة إلى الذاكرة
         'quiet': True,
         'cookiefile': 'cookies.txt',  # استخدام ملف الكوكيز لدعم الفيديوهات المحمية
-        'format': 'bestvideo+bestaudio/best',  # اختيار أفضل فيديو وصوت متاحين
+        'format': 'bestaudio',  # تحميل أفضل صوت فقط
         'noplaylist': True,  # لتجنب تحميل قوائم التشغيل
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(url, download=True)
-        video_url = result['formats'][0]['url']
-        video_data = yt_dlp.request.urlopen(video_url).read()
-        return video_data
+        audio_url = result['formats'][0]['url']
+        audio_data = yt_dlp.request.urlopen(audio_url).read()
+        return audio_data
 
 # الحدث عند تلقي رسالة
 @client.on(events.NewMessage(pattern='/download'))
 async def handler(event):
     try:
-        # استخدم الرابط المرسل لتحميل الفيديو
+        # استخدم الرابط المرسل لتحميل الصوت
         url = event.message.text.split(' ', 1)[1]
         
-        await event.respond('جارٍ تحميل الفيديو...')
+        await event.respond('جارٍ تحميل الصوت...')
 
-        # تحميل الفيديو إلى الذاكرة
-        video_data = await download_video(url)
+        # تحميل الصوت إلى الذاكرة
+        audio_data = await download_audio(url)
         
-        # إرسال الفيديو مباشرة من الذاكرة
-        await event.respond(file=io.BytesIO(video_data), force_document=False)
+        # إرسال الصوت مباشرة من الذاكرة
+        await event.respond(file=io.BytesIO(audio_data), force_document=False, voice=True)
 
     except IndexError:
         await event.respond('الرجاء إرسال رابط الفيديو بعد الأمر /download')
