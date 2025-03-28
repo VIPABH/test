4919c82964f1301a63e99dcd2f5561d22b7fe317
VIPABH <a891983.2@gmail.com>
2025-03-29 01:33:02 +0300

    Update p.py

--- p.py
+++ p.py
@@ -14,6 +14,37 @@ if not api_id or not api_hash or not bot_token:
 
 ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
+async def download_video(query: str):
+    ydl_opts = {
+        'format': 'worstaudio',
+        'quiet': True,
+        'noplaylist': True,
+        'cookiefile': 'cookies.txt',
+        'noprogress': True,
+        'extractaudio': True,
+        'default_search': 'ytsearch',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '64',
+            'nopostoverwrites': True,
+        }],
+        'progress_hooks': [lambda d: None],
+        'concurrent_fragment_downloads': 10,
+        'max_filesize': 50 * 1024 * 1024,
+        'socket_timeout': 30,
+    }
+    if not query.startswith(("http://", "https://")):
+        query = f"ytsearch:{query}"
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(query, download=True)
+        if 'entries' in info:
+            info = info['entries'][0]
+        output_file = ydl.prepare_filename(info)
+        audio_file = output_file.rsplit('.', 1)[0] + ".mp3"
+        if os.path.exists(audio_file) and os.path.getsize(audio_file) > 0:
+            return audio_file
 async def download_audio(query: str):
     ydl_opts = {
         'format': 'worstaudio',
@@ -91,26 +122,26 @@ async def handler_audio(event):
         os.remove(audio_file)
     else:
         await event.respond("فشل تحميل الصوت.")
-@ABH.on(events.NewMessage(pattern='فيديو'))
-async def handler_video(event):
+@ABH.on(events.NewMessage(pattern='فيديو|فديو'))
+async def handler(event):
     msg = await event.reply('🤌')
     msg_parts = event.message.text.split(' ', 1)
     if len(msg_parts) < 2:
         return await event.respond('ارسل الرابط أو النص المطلوب.')
     query = msg_parts[1]
-    video_file = await download_video(query)
-    if video_file:
+    audio_file = await download_video(query)
+    if audio_file:
         button = [Button.url("chanel", "https://t.me/sszxl")]
         await msg.delete()
         await event.client.send_file(
             event.chat_id, 
-            video_file, 
+            audio_file, 
             caption='**[Enjoy dear]**(https://t.me/VIPABH_BOT)', 
             buttons=button, 
             reply_to=event.message.id
         )
-        os.remove(video_file)
+        os.remove(audio_file)
     else:
-        await event.respond("فشل تحميل الفيديو.")
+        await event.respond("فشل تحميل الصوت.")
 
 ABH.run_until_disconnected()
