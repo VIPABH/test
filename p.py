7b0b194e8ba8d1d4ecaffb1739e39ecefeffbd1a
VIPABH <a891983.2@gmail.com>
2025-03-29 03:07:56 +0300

    Update p.py

--- p.py
+++ p.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 import os
 from telethon import events, TelegramClient
 from telethon.tl.custom import Button
+api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
