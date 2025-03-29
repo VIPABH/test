5361499ad64ce901eb0d3c655ff031cd087aacdd
VIPABH <a891983.2@gmail.com>
2025-03-30 01:11:57 +0300

    Update p.py

--- p.py
+++ p.py
@@ -453,7 +453,7 @@ async def yt_search(event):
     try:
         full_response = await ytsearch(query, limit=lim)
     except Exception as e:
-        return await event.reply(video_q, str(e), time=10, parse_mode=format.parse_pre)
+       return await event.reply(str(e))
     reply_text = f"**•  البحث المطلوب:**\n`{query}`\n\n**•  النتائج:**\n{full_response}"
     await event.reply(video_q, reply_text)
 ABH.run_until_disconnected()
