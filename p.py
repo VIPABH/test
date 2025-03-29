dd24c63645fa8f2ea93ffdafcc30de17814d651c
VIPABH <a891983.2@gmail.com>
2025-03-30 01:07:31 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,10 +1,16 @@
-import os, time
-from telethon import TelegramClient, events
-from youtubesearchpython import VideosSearch
+from telethon.tl.types import MessageEntityPre
+from telethon.utils import add_surrogate
+
+import asyncio
+import io
+import os
 import pathlib
+import re
+import time
 from telethon.tl import types
 from telethon.utils import get_attributes
 from youtube_dl import YoutubeDL
+from telethon import events, TelegramClient
 from youtube_dl.utils import (
     ContentTooShortError,
     DownloadError,
@@ -15,10 +21,28 @@ from youtube_dl.utils import (
     UnavailableVideoError,
     XAttrMetadataError,
 )
+from youtubesearchpython import VideosSearch
+
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN')
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+
+async def ytsearch(query, limit):
+    result = ""
+    videolinks = VideosSearch(query.lower(), limit=limit)
+    for v in videolinks.result()["result"]:
+        textresult = f"[{v['title']}](https://www.youtube.com/watch?v={v['id']})\n"
+        try:
+            textresult += f"**الشرح : **`{v['descriptionSnippet'][-1]['text']}`\n"
+        except Exception:
+            textresult += "**الشرح : **`None`\n"
+        textresult += (
+            f"**المدة : **{v['duration']}  **المشاهدات : **{v['viewCount']['short']}\n"
+        )
+        result += f"☞ {textresult}\n"
+    return result
+
 audio_opts = {
     "format": "bestaudio",
     "addmetadata": True,
@@ -90,6 +114,155 @@ async def ytdl_down(event, opts, url):
         return None
     return ytdl_data
 
+# Copyright (C) 2020 Adek Maulana
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import hashlib
+import math
+import re
+import time
+from typing import Dict, Tuple
+
+from telethon.errors.rpcerrorlist import MessageNotModifiedError
+_TASKS: Dict[str, Tuple[int, int]] = {}
+async def md5(fname: str) -> str:
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def humanbytes(size: int) -> str:
+    if size is None or isinstance(size, str):
+        return ""
+
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return f"{str(round(size, 2))} {dict_power_n[raised_to_pow]}B"
+
+
+def time_formatter(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    seconds = round(seconds, 2)
+    tmp = (
+        (f"{str(days)} day(s), " if days else "")
+        + (f"{str(hours)} hour(s), " if hours else "")
+        + (f"{str(minutes)} minute(s), " if minutes else "")
+        + (f"{str(seconds)} second(s), " if seconds else "")
+    )
+
+    return tmp[:-2]
+
+
+def readable_time(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    return (
+        (f"{int(days)} day(s), " if days else "")
+        + (f"{int(hours)}:" if hours else "00:")
+        + (f"{int(minutes)}:" if minutes else "00:")
+        + (str(int(seconds)) if seconds else "00")
+    )
+
+
+def human_to_bytes(size: str) -> int:
+    units = {
+        "M": 2**20,
+        "MB": 2**20,
+        "G": 2**30,
+        "GB": 2**30,
+        "T": 2**40,
+        "TB": 2**40,
+    }
+
+    size = size.upper()
+    if not re.match(r" ", size):
+        size = re.sub(r"([KMGT])", r" \1", size)
+    number, unit = [string.strip() for string in size.split()]
+    return int(float(number) * units[unit])
+
+
+class CancelProcess(Exception):
+    """
+    Cancel Process
+    """
+async def progress(
+    current,
+    total,
+    gdrive,
+    start,
+    prog_type,
+    file_name=None,
+    is_cancelled=False,
+    delay=5,
+):  # sourcery no-metrics
+    if is_cancelled is True:
+        raise CancelProcess
+    task_id = f"{gdrive.chat_id}.{gdrive.id}"
+    if current == total:
+        if task_id not in _TASKS:
+            return
+        del _TASKS[task_id]
+        try:
+            await gdrive.edit("`finalizing process ...`")
+        except MessageNotModifiedError:
+            pass
+        except Exception as e:
+            return
+    now = time.time()
+    if task_id not in _TASKS:
+        _TASKS[task_id] = (now, now)
+    start, last = _TASKS[task_id]
+    elapsed_time = now - start
+    oldtmp = ""
+    if (now - last) >= delay:
+        _TASKS[task_id] = (start, now)
+        percentage = current * 100 / total
+        speed = current / elapsed_time
+        eta = round((total - current) / speed)
+        elapsed_time = round(elapsed_time)
+        if "upload" in prog_type.lower():
+            status = "Uploading"
+        elif "download" in prog_type.lower():
+            status = "Downloading"
+        else:
+            return
+        tmp = (
+            f"`{humanbytes(current)} of {humanbytes(total)}"
+            f" @ {humanbytes(speed)}`\n"
+            f"**ETA :**` {time_formatter(eta)}`\n"
+            f"**Duration :** `{time_formatter(elapsed_time)}`"
+        )
+        if tmp != oldtmp:
+            if file_name:
+                await gdrive.edit(
+                    f"**{prog_type}**\n\n"
+                    f"**File Name : **`{file_name}`**\nStatus**\n{tmp}"
+                )
+            else:
+                await gdrive.edit(f"**{prog_type}**\n\n" f"**Status**\n{tmp}")
+            oldtmp = tmp
 
 async def fix_attributes(
     path, info_dict: dict, supports_streaming: bool = False, round_message: bool = False
@@ -138,23 +311,126 @@ async def fix_attributes(
         ):
             new_attributes.append(attr)
     return new_attributes, mime_type
+
+
 async def get_file_name(path: pathlib.Path, full: bool = True) -> str:
     return str(path.absolute()) if full else path.stem + path.suffix
 
-async def ytsearch(query, limit):
-    result = ""
-    videolinks = VideosSearch(query.lower(), limit=limit)
-    for v in videolinks.result()["result"]:
-        textresult = f"[{v['title']}](https://www.youtube.com/watch?v={v['id']})\n"
-        try:
-            textresult += f"**الشرح : **`{v['descriptionSnippet'][-1]['text']}`\n"
-        except Exception:
-            textresult += "**الشرح : **`None`\n"
-        textresult += (
-            f"**المدة : **{v['duration']}  **المشاهدات : **{v['viewCount']['short']}\n"
-        )
-        result += f"☞ {textresult}\n"
-    return result
+
+@ABH.on(events.NewMessage(pattern=r"فيديو(?: |$)(\d*)? ?([\s\S]*)"))
+async def download_audio(event):
+    """To download audio from YouTube and many other sites."""
+    url = event.pattern_match.group(1)
+    rmsg = await event.get_reply_message()
+    if not url and rmsg:
+        myString = rmsg.text
+        url = re.search("(?P<url>https?://[^\s]+)", myString).group("url")
+    if not url:
+        return await event.reply(event, "᯽︙ - يجب وضع رابط لتحميله ❕")
+    catevent = await event.reply(event, "᯽︙ يتم الاعداد انتظر")
+    ytdl_data = await ytdl_down(catevent, audio_opts, url)
+    if ytdl_data is None:
+
+        return
+    await catevent.edit(
+        f"᯽︙ يتم لتحميل الأغنية:\
+        \n᯽︙ {ytdl_data['title']}\
+        \nبواسطة ᯽︙ {ytdl_data['uploader']}"
+    )
+    f = pathlib.Path(f"{ytdl_data['title']}.mp3".replace("|", "_"))
+    catthumb = pathlib.Path(f"{ytdl_data['title']}.mp3.jpg".replace("|", "_"))
+    if not os.path.exists(catthumb):
+        catthumb = pathlib.Path(f"{ytdl_data['title']}.mp3.webp".replace("|", "_"))
+    if not os.path.exists(catthumb):
+        catthumb = None
+    c_time = time.time()
+    ul = io.open(f, "rb")
+    uploaded = await event.client.fast_upload_file(
+        file=ul,
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, catevent, c_time, "upload", file_name=f)
+        ),
+    )
+    ul.close()
+    attributes, mime_type = await fix_attributes(f, ytdl_data, supports_streaming=True)
+    media = types.InputMediaUploadedDocument(
+        file=uploaded,
+        mime_type=mime_type,
+        attributes=attributes,
+        thumb=await event.client.upload_file(catthumb) if catthumb else None,
+    )
+    await event.client.send_file(
+        event.chat_id,
+        file=media,
+        reply_to=event.reply,
+        caption=ytdl_data["title"],
+        supports_streaming=True,
+        force_document=False,
+    )
+    os.remove(f)
+    if catthumb:
+        os.remove(catthumb)
+    await catevent.delete()
+
+@ABH.on(events.NewMessage(pattern=r"فديو(?: |$)(\d*)? ?([\s\S]*)"))
+async def download_video(event):
+    """To download video from YouTube and many other sites."""
+    url = event.pattern_match.group(1)
+    rmsg = await event.get_reply_message()
+    if not url and rmsg:
+        myString = rmsg.text
+        url = re.search("(?P<url>https?://[^\s]+)", myString).group("url")
+    if not url:
+        return await event.reply(event, "᯽︙ عـليك وضع رابـط اولا ليتـم تنـزيله")
+    catevent = await event.reply(event, "᯽︙ يتم التحميل انتظر قليلا")
+
+    ytdl_data = await ytdl_down(catevent, video_opts, url)
+    if ytdl_down is None:
+        return
+    f = pathlib.Path(f"{ytdl_data['title']}.mp4".replace("|", "_"))
+    catthumb = pathlib.Path(f"{ytdl_data['title']}.jpg".replace("|", "_"))
+    if not os.path.exists(catthumb):
+        catthumb = pathlib.Path(f"{ytdl_data['title']}.webp".replace("|", "_"))
+    if not os.path.exists(catthumb):
+        catthumb = None
+    await catevent.edit(
+        f"᯽︙ التحضيـر للـرفع انتظر:\
+        \n᯽︙ {ytdl_data['title']}\
+        \nبـواسطة *{ytdl_data['uploader']}*"
+    )
+    ul = io.open(f, "rb")
+    c_time = time.time()
+    attributes, mime_type = await fix_attributes(f, ytdl_data, supports_streaming=True)
+    uploaded = await event.client.fast_upload_file(
+        file=ul,
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, catevent, c_time, "upload", file_name=f)
+        ),
+    )
+    ul.close()
+    media = types.InputMediaUploadedDocument(
+        file=uploaded,
+        mime_type=mime_type,
+        attributes=attributes,
+        thumb=await event.client.upload_file(catthumb) if catthumb else None,
+    )
+    await event.client.send_file(
+        event.chat_id,
+        file=media,
+        reply_to=event.reply,
+        caption=ytdl_data["title"],
+    )
+    os.remove(f)
+    if catthumb:
+        os.remove(catthumb)
+    await event.delete()
+    
+def parse_pre(text):
+    text = text.strip()
+    return (
+        text,
+        [MessageEntityPre(offset=0, length=len(add_surrogate(text)), language="")],
+    )
 @ABH.on(events.NewMessage(pattern=r"يوت(?: |$)(\d*)? ?([\s\S]*)"))
 async def yt_search(event):
     "Youtube search command"
@@ -167,7 +443,7 @@ async def yt_search(event):
         return await event.reply(
             event, "**᯽︙ قم بالرد على النص او كتابته مع الامر**"
         )
-    video_q = await event.reply("**᯽︙ يتم البحث في اليوتيوب**")
+    video_q = await event.reply(event, "**᯽︙ يتم البحث في اليوتيوب**")
     if event.pattern_match.group(1) != "":
         lim = int(event.pattern_match.group(1))
         if lim <= 0:
@@ -177,7 +453,7 @@ async def yt_search(event):
     try:
         full_response = await ytsearch(query, limit=lim)
     except Exception as e:
-        return await event.reply(str(e))
+        return await event.reply(video_q, str(e), time=10, parse_mode=format.parse_pre)
     reply_text = f"**•  البحث المطلوب:**\n`{query}`\n\n**•  النتائج:**\n{full_response}"
-    await event.reply(reply_text)
+    await event.reply(video_q, reply_text)
 ABH.run_until_disconnected()
