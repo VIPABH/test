19abea9fcf1fad8d936d4e860ae9ec86948eae65
VIPABH <a891983.2@gmail.com>
2025-04-07 02:01:59 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,10 +1,15 @@
 import os
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageEntityUrl
+
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+
+x = set()  # تم تصحيحها من dict إلى set
+
 @ABH.on(events.MessageEdited)
 async def test(event):
     msg = event.message
@@ -12,8 +17,16 @@ async def test(event):
     has_document = bool(msg.document)
     has_url = any(isinstance(entity, MessageEntityUrl) for entity in (msg.entities or []))
     perms = await ABH.get_permissions(event.chat_id, event.sender_id)
-    if (has_media or has_document or has_url) and not (perms.is_admin or perms.is_creator):
+    uid = event.sender_id
+
+    if (has_media or has_document or has_url) and not (perms.is_admin or perms.is_creator or uid in x):
         await event.reply('ها شعدلت ولك!.')
     else:
         return
+
+@ABH.on(events.NewMessage(pattern='سماح'))
+async def سماح(event):  # تم تغيير اسم الدالة لتفادي تعارض الاسم مع المتغير x
+    uid = event.sender_id
+    x.add(uid)
+
 ABH.run_until_disconnected()
