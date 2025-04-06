b44dca9de7d52742256dd105de76bd3f27a8e791
VIPABH <a891983.2@gmail.com>
2025-04-07 01:39:33 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,24 +1,19 @@
-from telethon import TelegramClient, events
 import os
-
-api_id = int(os.getenv('API_ID'))
+from telethon import TelegramClient, events
+from telethon.tl.types import MessageEntityUrl
+api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-
-ABH = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
-
-@ABH.on(events.NewMessage(pattern='/check'))
-async def check_admin_status(event):
-    try:
-        perms = await ABH.get_permissions(event.chat_id, event.sender_id)
-
-        if perms.is_creator:
-            await event.reply("👑 أنت المالك (Creator).")
-        elif perms.is_admin:
-            await event.reply("🛡️ أنت مشرف (Admin).")
-        else:
-            await event.reply("👤 أنت عضو عادي.")
-    except Exception as e:
-        await event.reply(f"⚠️ لم أتمكن من التحقق.\nالسبب: {str(e)}")
-
+ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+@ABH.on(events.MessageEdited)
+async def test(event):
+    msg = event.message
+    has_media = bool(msg.media)
+    has_document = bool(msg.document)
+    has_url = any(isinstance(entity, MessageEntityUrl) for entity in (msg.entities or []))
+    perms = await ABH.get_permissions(event.chat_id, event.sender_id)
+    if (has_media or has_document or has_url) and not (perms.is_admin or perms.is_creator):
+        await event.reply('ها شعدلت ولك!.')
+    else:
+        return
 ABH.run_until_disconnected()
