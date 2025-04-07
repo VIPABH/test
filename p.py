030e57348a7e598ded80ef038adb1143fa7032d9
VIPABH <a891983.2@gmail.com>
2025-04-07 14:47:50 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,13 +1,10 @@
 import os, asyncio
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageEntityUrl
-
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-
 x = set()
 hint_gid = -1002168230471
 @ABH.on(events.MessageEdited)
@@ -23,21 +20,14 @@ async def test(event):
     uid = event.sender_id
     if uid in x:
         return
-
     if (has_media or has_document or has_url) and not (perms.is_admin or perms.is_creator or uid in x):
         sender = await event.get_sender()
         nid = sender.first_name
         msg_link = f"https://t.me/{event.chat.username}/{event.id}" if event.chat.username else None
         uid = event.sender_id
         await ABH.send_message(hint_gid, f'تم تعديل رسالة مريبة \n رابط الرسالة ↢ `{msg_link}` \n ايدي المستخدم ↢ `{uid}` \n اسم المستخدم ↢ `{nid}`')
-        await asyncio.sleep(3)
+        await asyncio.sleep(60)
         await event.delete()
     else:
         return
-
-@ABH.on(events.NewMessage(pattern='سماح'))
-async def سماح(event):
-    uid = event.sender_id
-    x.add(uid)
-
 ABH.run_until_disconnected()
