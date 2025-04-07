31704b811ffc7023c4a59f0c8ec418087c523566
VIPABH <a891983.2@gmail.com>
2025-04-07 16:00:08 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,33 +1,54 @@
 import os, asyncio
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageEntityUrl
+from telethon.errors import SessionPasswordNeededError
+
+# تحميل المتغيرات البيئية
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+
+# إنشاء عميل بوت
+ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+# مجموعة لتخزين المستخدمين الذين تم التعامل معهم
 x = set()
+
+# معرف مجموعة الإبلاغ
 hint_gid = -1002168230471
+
 @ABH.on(events.MessageEdited)
 async def test(event):
     chat = event.chat_id
-    if chat != -1001784332159:
+    if chat != -1001784332159:  # تحقق من المحادثة المستهدفة
         return  
+
     msg = event.message
     has_media = bool(msg.media)
     has_document = bool(msg.document)
     has_url = any(isinstance(entity, MessageEntityUrl) for entity in (msg.entities or []))
+    
+    # الحصول على أذونات المستخدم
     perms = await ABH.get_permissions(event.chat_id, event.sender_id)
     uid = event.sender_id
+
+    # إذا كان المستخدم في مجموعة x لا تتابع الرسالة
     if uid in x:
         return
+
+    # التحقق من أن الرسالة تم تعديلها فعلاً
     if (has_media or has_document or has_url) and not (perms.is_admin or perms.is_creator or uid in x):
         sender = await event.get_sender()
         nid = sender.first_name
         msg_link = f"https://t.me/{event.chat.username}/{event.id}" if event.chat.username else None
         uid = event.sender_id
+
+        # إرسال إشعار
         await ABH.send_message(hint_gid, f'تم #تعديل رسالة مريبة \n رابط الرسالة ↢ `{msg_link}` \n ايدي المستخدم ↢ `{uid}` \n اسم المستخدم ↢ `{nid}`')
-        await asyncio.sleep(60)
-        await event.delete()
+        await asyncio.sleep(60)  # الانتظار 60 ثانية قبل الحذف
+        await event.delete()  # حذف الرسالة المعدلة
     else:
         return
+
+# تشغيل البوت
 ABH.run_until_disconnected()
