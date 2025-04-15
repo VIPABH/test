99cce17af47697847516e39b3a4f3ec918b42fce
VIPABH <a891983.2@gmail.com>
2025-04-15 18:28:54 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,10 +9,10 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 # تشغيل العميل
 bot = TelegramClient('bot_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-# معرف المستخدم أو البوت المستهدف
-TARGET_USER_ID = 1421907917
-
-@bot.on(events.NewMessage(pattren="مالي خلقك"))
+# المعالج لحذف الرسائل التي تحتوي على "مالي خلقك"
+@bot.on(events.NewMessage(pattern="مالي خلقك"))
 async def delete_target_messages(event):
     await event.delete()
+
+# تشغيل البوت حتى يتم إيقافه
 bot.run_until_disconnected()
