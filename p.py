93b3f2a8217646a47ff06646e908026d2e9ef3e6
VIPABH <a891983.2@gmail.com>
2025-04-17 01:48:22 +0300

    Update p.py

--- p.py
+++ p.py
@@ -90,7 +90,7 @@ async def handler(event):
         photo_path = os.path.join(LOCAL_PHOTO_DIR, f"{user_id}.jpg")
         await ABH.download_profile_photo(user.id, file=photo_path)
         msg = await ABH.send_file(event.chat_id, photo_path, caption=message_text, force_document=False)
-        await asyncio.sleep(2)
+        await asyncio.sleep(60*3)
         await msg.delete()
     else:
         await event.respond(message_text)
@@ -123,7 +123,7 @@ async def handler(event):
         photo_path = os.path.join(LOCAL_PHOTO_DIR, f"{user_id}.jpg")
         await ABH.download_profile_photo(user.id, file=photo_path)
         msg = await ABH.send_file(event.chat_id, photo_path, caption=message_text, force_document=False)
-        await asyncio.sleep(2)
+        await asyncio.sleep(60*3)
         await msg.delete()
     else:
         await event.respond(message_text)
