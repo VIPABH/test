fb90eb19f4f8e89c88779ee847fb76d88c647285
VIPABH <a891983.2@gmail.com>
2025-04-16 15:10:53 +0300

    Update p.py

--- p.py
+++ p.py
@@ -11,8 +11,16 @@ ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @ABH.on(events.NewMessage)
 async def e(event):
     message_text = '>>>>'
+    # Send the video file
     msg = await ABH.send_file(event.chat_id, file='https://files.catbox.moe/k44qq6.mp4', caption=message_text)
     await asyncio.sleep(2)
+    
+    # Edit the message caption
     message_text = '...'
-    await msg.edit(message_text)
+    await msg.edit(caption=message_text)
+    
+    # Delete the message (video) after editing
+    await asyncio.sleep(2)  # Optionally wait before deletion
+    await ABH.delete_messages(event.chat_id, msg)
+
 ABH.run_until_disconnected()
