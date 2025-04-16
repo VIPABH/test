d61ece23d58a1dd778bcc1e46ff840d2502e8631
VIPABH <a891983.2@gmail.com>
2025-04-16 15:08:20 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,22 +1,19 @@
 from telethon import TelegramClient, events
 import os, asyncio
-import aiohttp #type: ignore
 from datetime import datetime
-from telethon.tl.types import ChannelParticipant, ChannelParticipantAdmin, ChannelParticipantCreator
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.functions.channels import GetParticipantRequest
-
 
 api_id = int(os.getenv('API_ID', '123456'))
 api_hash = os.getenv('API_HASH', 'your_api_hash')
 bot_token = os.getenv('BOT_TOKEN', 'your_bot_token')
+
 ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
 @ABH.on(events.NewMessage)
 async def e(event):
     message_text = '>>>>'
     msg = await ABH.send_file(event.chat_id, photo='https://files.catbox.moe/k44qq6.mp4', caption=message_text)
     await asyncio.sleep(2)
     message_text = '...'
-    await msg.edit(event.chat_id, caption=message_text)
+    await msg.edit(caption=message_text)
 
 ABH.run_until_disconnected()
