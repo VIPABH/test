e912edd3ee027a16fa21529459ac50ca48fdd3bb
VIPABH <a891983.2@gmail.com>
2025-04-22 13:49:00 +0300

    Update p.py

--- p.py
+++ p.py
@@ -7,16 +7,14 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN')
 
-# إنشاء العميل (البوت)
 ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-CHANNEL_ID = -1002116581783  # تأكد من أن هذا هو ID القناة أو اسمها الصحيح (مثلاً '@channel_username')
-
-# دالة التحقق من الاشتراك في القناة باستخدام API الخاص بالبوت
+CHANNEL_ID = -1002116581783
+الرابط = "https://t.me/x04ou"
 def is_user_subscribed(user_id):
     url = f"https://api.telegram.org/bot{bot_token}/getChatMember?chat_id={CHANNEL_ID}&user_id={user_id}"
     try:
         response = requests.get(url).json()
-        print("Response:", response)  # طباعة الرد الكامل للتفاصيل
+        print("Response:", response)
         if response.get("ok"):
             status = response["result"]["status"]
             print(f"User status: {status}")
@@ -27,26 +25,17 @@ def is_user_subscribed(user_id):
     except requests.exceptions.RequestException as e:
         print(f"Error making request: {e}")
         return False
-
-# مراقبة الرسائل الخاصة فقط
 @ABH.on(events.NewMessage(incoming=True))
 async def handler(event):
     if not event.is_private:
         return
-
     user_id = event.sender_id
     if not is_user_subscribed(user_id):
-        # إذا لم يكن مشتركًا في القناة، أرسل رسالة اشتراك
-        channel_link = f"https://t.me/{CHANNEL_ID}"
         await event.respond(
-            f"📌 للمتابعة، يرجى الاشتراك أولاً في القناة:\n{channel_link}",
-            buttons=[Button.url("اضغط هنا للاشتراك", channel_link)]
+            f"📌 للمتابعة، يرجى الاشتراك أولاً في القناة:\n{الرابط}",
+            buttons=[Button.url("اضغط هنا للاشتراك", الرابط)]
         )
         await event.delete()
         return
-
-    # إذا كان مشتركًا بالفعل، يمكنه استخدام البوت
     await event.respond("✅ مرحباً بك، أنت مشترك ويمكنك استخدام البوت.")
-
-# تشغيل البوت
 ABH.run_until_disconnected()
