8ee1444473924d61834a67f70008613dd66576c5
VIPABH <a891983.2@gmail.com>
2025-04-24 02:43:11 +0300

    Update p.py

--- p.py
+++ p.py
@@ -10,7 +10,7 @@ API_HASH_3 = os.getenv("API_HASH_4")
 
 if API_ID_3 and API_HASH_3:
     session_name = "session_4"
-    client = TelegramClient(session_name, int(API_ID_3), API_HASH_3)
+    client = TelegramClient(session_name, int(API_ID_4), API_HASH_4)
 
     target_user_id = None
     selected_emojis = []
