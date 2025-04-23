0d7dafeb186215f38a64388d453f46bd2c3eb6dd
VIPABH <a891983.2@gmail.com>
2025-04-24 02:46:30 +0300

    Update p.py

--- p.py
+++ p.py
@@ -4,12 +4,12 @@ from telethon.tl.functions.messages import SendReactionRequest
 from telethon.tl.types import ReactionEmoji
 
 
-API_ID_3 = os.getenv("API_ID_5")
-API_HASH_3 = os.getenv("API_HASH_5")
+API_ID_3 = os.getenv("API_ID_6")
+API_HASH_3 = os.getenv("API_HASH_6")
 
 
 if API_ID_3 and API_HASH_3:
-    session_name = "session_5"
+    session_name = "session_6"
     client = TelegramClient(session_name, int(API_ID_3), API_HASH_3)
 
     target_user_id = None
