fd734c64376c634f348966b5ba29b8c0e1165f05
VIPABH <a891983.2@gmail.com>
2025-04-26 23:57:03 +0300

    Update p.py

--- p.py
+++ p.py
@@ -10,7 +10,7 @@ from telebot import types
 YOUTUBE_API_KEY = 'AIzaSyDLp3YbxDpGMGHmGS7Kx39GLqHmYJ5b8XE'
 YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(bot_token)
 
 cooldown = {}
 logging.basicConfig(filename='errors.log', level=logging.ERROR, format='%(asctime)s - %(message)s')
