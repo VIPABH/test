c9fe1ac9438d5462a37142d7e81c5c57744fac92
VIPABH <a891983.2@gmail.com>
2025-05-05 12:14:08 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,18 +1,18 @@
 from telethon import TelegramClient, events
-import re
-import asyncio
-import os
+import asyncio, os
 
-# إعدادات API
-api_id = os.getenv('API_ID')      
-api_hash = os.getenv('API_HASH')  
-bot_token = os.getenv('BOT_TOKEN')
+api_id = int(os.getenv('API_ID'))
+api_hash = os.getenv('API_HASH')
+
 ABH = TelegramClient('code', api_id, api_hash)
+
 @ABH.on(events.NewMessage(outgoing=True))
-async def pin(event):
+async def handle_outgoing(event):
     await event.reply('Hello, this is a test message!')
-@ABH.on(events.NewMessage(func=lambda e: e.is_private, incoming=True))
-async def pin(event):
-    await event.reply('Hello, !')
+
+@ABH.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def handle_private(event):
+    await event.reply('Hello!')
 
 ABH.start()
+ABH.run_until_disconnected()
