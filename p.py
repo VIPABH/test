aedbff3f47a9211d0253c904bd8e4fb1478394b5
VIPABH <a891983.2@gmail.com>
2025-05-07 16:51:15 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,17 +1,14 @@
 from telethon import TelegramClient, events
 import os, random, time
-
 api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-
 ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 questions_and_answers_q = [
     {"question": "https://t.me/LANBOT2/90", "answer": "محمد صلاح"}
 ]
 states = {}
-
 @ABH.on(events.NewMessage(pattern='/start'))
 async def quest(event):
     """بدء السؤال العشوائي"""
@@ -22,51 +19,30 @@ async def quest(event):
         "waiting_for_answer": True,
         "start_time": time.time()
     }
-
-    # تحميل الصورة عبر ABH.download
-    link = quest['question']
-    try:
-        # تحميل الرابط (الصورة) إلى ملف محلي
-        downloaded_file = await ABH.download_media(link, file="downloaded_image.jpg")
-        
-        # إرسال الصورة بعد التحميل
-        await event.reply(f"السؤال: من هو اللاعب الذي تم تصويره؟")
-        await ABH.send_file(event.chat_id, downloaded_file, caption="إليك الصورة!")
-
-    except Exception as e:
-        await event.reply("❌ فشل في تحميل الصورة.")
-
+    await event.reply(f"{quest['question']}")
 @ABH.on(events.NewMessage)
 async def check_quist(event):
     if not event.text:
         return
-    
     user_id = event.sender_id
     usermessage = event.text.strip()
     gid = event.chat_id
-    
     if user_id in states and states[user_id].get("waiting_for_answer"):
         question_q = states[user_id].get("question", {})
-        answers_q = [question_q.get('answer', '')]
+        answers_q = question_q.get('answer', [])
         start_time = states[user_id].get("start_time")
         current_time = time.time()
         time_passed = current_time - start_time
-        
-        # تحقق من انقضاء الوقت
         if time_passed > 60:
             del states[user_id]
             return
-        
-        # تحقق من الإجابة الصحيحة
         if usermessage in answers_q:
             p = random.randint(50, 500)
-            # يمكنك إضافة النقاط هنا عند الحاجة
             # add_points(user_id, gid, points, amount=p)
             await event.reply(
-                f"✅ إجابة صحيحة! ربحت `{p}` نقاط!"
+                "هلا هلا طبوا الشيعة 🫡 \n ربحت (`{p}`) \n فلوسك ↢ {points[str(user_id)][str(gid)]['points']}"
             )
             del states[user_id]
         else:
-            await event.reply("❌ إجابة خاطئة. حاول مرة أخرى.")
-
+            pass
 ABH.run_until_disconnected()
