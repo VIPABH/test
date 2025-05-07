61445c8a8e01141b5d3e89a20fa7b8a9d4248a47
VIPABH <a891983.2@gmail.com>
2025-05-07 16:57:43 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,6 +1,11 @@
 from telethon import events
 from telethon.tl.types import InputPeerPhotoFileLocation
 import asyncio
+api_id = int(os.getenv('API_ID'))
+api_hash = os.getenv('API_HASH')
+bot_token = os.getenv('BOT_TOKEN')
+client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
 
 football = [
     {
