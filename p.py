f9e3035d62f96482a5f06aff8d52c13ae2872d0b
VIPABH <a891983.2@gmail.com>
2025-05-15 23:17:48 +0300

    Update p.py

--- p.py
+++ p.py
@@ -37,7 +37,7 @@ bot_token = os.getenv('BOT_TOKEN')
 client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
 
 # أمر ذكاء
-@client.on(events.NewMessage(pattern=r"^ذكاء\s*(.*)", func=lambda e: e.is_group))
+@client.on(events.NewMessage(pattern=r"^ذكاء\s*(.*)"))
 async def ai_handler(event):
     user_q = event.pattern_match.group(1).strip()
     if not user_q:
