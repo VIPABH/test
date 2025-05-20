6f7e32333a85f506aefe159f19d919f895ab2e0e
VIPABH <a891983.2@gmail.com>
2025-05-20 22:46:35 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,12 +1,23 @@
 from telethon import TelegramClient, events
-import os
+import os 
+from Resources import mention
 
 SESSION='session'
 API_ID=int(os.getenv("API_ID"))
 API_HASH=os.getenv("API_HASH")
 BOT_TOKEN=os.getenv("BOT_TOKEN")
-
 ABH=TelegramClient(SESSION,API_ID,API_HASH).start(bot_token=BOT_TOKEN)
+@ABH.one(events.NewMessage(pattern="^اسمي$"))
+async def myname(event):
+ s = await event.get_sender()
+ name = await mention(event, s)
+ await event.reply(name)
+@ABH.one(events.NewMessage(pattern="^اسمه|اسمة$"))
+async def myname(event):
+ r = await event.get_reply_message()
+ s = await r.get_sender()
+ name = await mention(event, s)
+ await event.reply(name)
 @ABH.on(events.NewMessage(pattern="^رقمي$"))
 async def handler(event):
  s=await event.get_sender()
