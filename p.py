356a670054d94eabadb9783cddb5af3c08ee0833
VIPABH <a891983.2@gmail.com>
2025-05-20 21:22:43 +0300

    Update p.py

--- p.py
+++ p.py
@@ -9,13 +9,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ABH = TelegramClient(SESSION, API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 @ABH.on(events.NewMessage(pattern="^يوزري$"))
 async def handler(event):
-    s = await event.get_sender()
-    u = s.username
-    await event.reply(f"`@{u}` @{u}")
+ s=await event.get_sender()
+
+ u=s.username or (s.usernames[0] if getattr(s,"usernames",None) else None)
+ await event.reply(f"`@{u}` @{u}" if u else "ليس لديك يوزر")
 @ABH.on(events.NewMessage(pattern="^يوزره|يوزرة|اليوزر$"))
 async def handler(event):
-    r = await event.get_reply_message()
-    s = await r.get_sender()
-    u = s.username
-    await event.reply(f"`@{u}` @{u}")
+ r=await event.get_reply_message()
+ s=await r.get_sender()
+ u=s.username or (s.usernames[0] if getattr(s,"usernames",None) else None)
+ await event.reply(f"`@{u}` @{u}" if u else "ليس لديه يوزر")
 ABH.run_until_disconnected()
