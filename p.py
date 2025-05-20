1b89aadbaf4de505ec6f088daebae16acc5994ff
VIPABH <a891983.2@gmail.com>
2025-05-20 22:09:24 +0300

    Update p.py

--- p.py
+++ p.py
@@ -8,6 +8,27 @@ BOT_TOKEN=os.getenv("BOT_TOKEN")
 
 ABH=TelegramClient(SESSION,API_ID,API_HASH).start(bot_token=BOT_TOKEN)
 
+@ABH.on(events.NewMessage(pattern="^يوزراتي$"))
+async def handler(event):
+ s=await event.get_sender()
+ usernames=[x.username for x in s.usernames] if getattr(s,"usernames",None) else []
+ if s.username: usernames.insert(0, s.username)
+ usernames=list(dict.fromkeys(usernames))
+ utext="\n".join(f"@{u}" for u in usernames)
+ await event.reply(utext if usernames else "ليس لديك أي يوزرات NFT")
+
+@ABH.on(events.NewMessage(pattern="^يوزراته$"))
+async def handler(event):
+ r=await event.get_reply_message()
+ if not r:
+  await event.reply("يجب الرد على رسالة المستخدم")
+  return
+ s=await r.get_sender()
+ usernames=[x.username for x in s.usernames] if getattr(s,"usernames",None) else []
+ if s.username: usernames.insert(0, s.username)
+ usernames=list(dict.fromkeys(usernames))
+ utext="\n".join(f"@{u}" for u in usernames)
+ await event.reply(utext if usernames else "ليس لديه أي يوزرات NFT")
 @ABH.on(events.NewMessage(pattern="^يوزري$"))
 async def handler(event):
  s=await event.get_sender()
