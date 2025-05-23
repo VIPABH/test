74ad242bcce1bf59a5e562a57fdfbddfa203b015
VIPABH <a891983.2@gmail.com>
2025-05-23 13:46:04 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient, events
+from Resources import mention
 from telethon.tl.functions.channels import GetParticipantRequest, EditBannedRequest
 from telethon.tl.types import ChannelParticipantCreator, ChannelParticipantAdmin, ChatBannedRights
 import os, time
@@ -7,21 +8,22 @@ api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 ABH = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
 restriction_end_times = {}
-@ABH.on(events.NewMessage(pattern='^تقييد عام$'))
+@ABH.on(events.NewMessage(pattern='^تقييد عام|مخفي قيده|مخفي قيدة$'))
 async def restrict_user(event):
     if not event.is_group:
-        return await event.reply(" هذا الأمر يعمل فقط في المجموعات.")
+        return
     r = await event.get_reply_message()
     if not r:
         return await event.reply(" يجب الرد على رسالة العضو الذي تريد تقييده.")    
     chat = await event.get_chat()
-    sender = await event.get_sender()
+    sender = await r.get_sender()
+    name = await mention(event, sender)
     try:
         participant = await ABH(GetParticipantRequest(channel=chat.id, participant=sender.id))
         if not isinstance(participant.participant, (ChannelParticipantCreator, ChannelParticipantAdmin)):
-            return await event.reply("أنت مشرف أو مالك، لا يمكنك استخدام هذا الأمر.")
+            return await event.reply(f"لا يمكنك تقييد {name} لانه مشرف ")
     except:
-        return await event.reply(" لم أتمكن من التحقق من صلاحياتك.")
+        return
     user_to_restrict = await r.get_sender()
     user_id = user_to_restrict.id
     now = int(time.time())
@@ -35,7 +37,7 @@ async def restrict_user(event):
         await ABH(EditBannedRequest(channel=chat.id, participant=user_id, banned_rights=rights))
         await event.reply(f" تم تقييد {user_to_restrict.first_name} لمدة 10 دقائق.")
     except Exception as e:
-        await event.reply(f" فشل التقييد")
+        await event.reply(f" ياريت اقيده بس ماكدر لان مشرف")
 @ABH.on(events.NewMessage)
 async def monitor_messages(event):
     if not event.is_group:
@@ -53,7 +55,7 @@ async def monitor_messages(event):
                     send_messages=True
                 )
                 await ABH(EditBannedRequest(channel=chat.id, participant=user_id, banned_rights=rights))
-                await event.reply(f" لا يمكنك إرسال الرسائل الآن. تم إعادة تقييدك لمدة {remaining//60} دقيقة و {remaining%60} ثانية.")
+                await event.reply(f" لا يمكنك إرسال الرسائل الآن. تم إعادة تقييدك لمدة ||{remaining//60} دقيقة و {remaining%60} ثانية.||")
             except:
                 pass
 ABH.run_until_disconnected()
