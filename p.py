2e0ad0d7c51cd4bde0b8f04177a2de5018dc3dc0
VIPABH <a891983.2@gmail.com>
2025-06-01 14:54:58 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 import shutil
-from pyrogram import Client, filters
+from telethon import TelegramClient, events
 from yt_dlp import YoutubeDL
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
@@ -25,28 +25,25 @@ YDL_OPTIONS = {
     }],
 }
 
-final = Client("youtube_audio_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-@final.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("مرحباً! أرسل:\n\nيوت + اسم الأغنية")
+ABH = TelegramClient("x", api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
 x = 1
-@final.on_message(filters.regex(r"^(يوت|yt) (.+)"))
+@ABH.on(events.NewMessage(pattern='يوت|yt (.*)'))
 async def download_audio(client, message):
     global x
-    query = message.text.split(" ", 1)[1]
+    query = message.pattern_match.group(1)
     ydl = YoutubeDL(YDL_OPTIONS)
     info = await asyncio.to_thread(ydl.extract_info, f"ytsearch:{query}", download=True)
     if 'entries' in info and len(info['entries']) > 0:
         info = info['entries'][0]
         file_path = ydl.prepare_filename(info).replace(".webm", ".mp3").replace(".m4a", ".mp3")
-        await client.send_audio(  
+        await ABH.send_audio(  
             chat_id=1910015590,
             audio=file_path,
-            title=info.get("title"),
+            title=f"ANYMOUS - {info.get('title', 'ABH')}",
             performer=info.get("uploader"),
             reply_to_message_id=message.id, 
             caption=f'{x}'  
         )
         x += 1
         os.remove(file_path)
-final.run()
+ABH.run_until_disconnected()
