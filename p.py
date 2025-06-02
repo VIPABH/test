4842362bc209e843a8efd75845c6a94e485b3c75
VIPABH <a891983.2@gmail.com>
2025-06-03 02:20:16 +0300

    Update p.py

--- p.py
+++ p.py
@@ -7,6 +7,7 @@ from yt_dlp import YoutubeDL
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+ABH = TelegramClient("x", api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
 if not os.path.exists("downloads"):
     os.makedirs("downloads")
 CACHE_FILE = "audio_cache.json"
@@ -23,43 +24,22 @@ YDL_OPTIONS = {
     'outtmpl': 'downloads/%(title)s.%(ext)s',
     'noplaylist': True,
     'quiet': True,
-    'cookiefile': "cookies.txt",
+    # 'cookiefile': "cookies.txt",
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '128',
     }],
 }
-ABH = TelegramClient("x", api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
 x = 1
 @ABH.on(events.NewMessage(pattern=r'^(يوت|yt) (.+)'))
 async def download_audio(event):
     global x
-    query = event.pattern_match.group(2)
-    ydl = YoutubeDL(YDL_OPTIONS)
-    for key, val in audio_cache.items():
-        if isinstance(val, dict) and val.get("query") == query:
-            await ABH.send_file(
-                1910015590,
-                file=val["file_id"],
-                caption=f"{x}",
-                attributes=[
-                    DocumentAttributeAudio(
-                        duration=val.get("duration", 0),
-                        title=val.get("title"),
-                        performer='ANYMOUS'
-                    )
-                ]
-            )
-            x += 1
-            return
-    info = await asyncio.to_thread(ydl.extract_info, f"ytsearch:{query}", download=False)
-    if 'entries' in info and len(info['entries']) > 0:
-        video_info = info['entries'][0]
-        video_id = video_info.get('id')
-        if video_id in audio_cache:
-            val = audio_cache[video_id]
-            if isinstance(val, dict):
+    try:
+        query = event.pattern_match.group(2)
+        ydl = YoutubeDL(YDL_OPTIONS)
+        for key, val in audio_cache.items():
+            if isinstance(val, dict) and val.get("query") == query:
                 await ABH.send_file(
                     1910015590,
                     file=val["file_id"],
@@ -74,28 +54,54 @@ async def download_audio(event):
                 )
                 x += 1
                 return
-    info = await asyncio.to_thread(ydl.extract_info, f"ytsearch:{query}", download=True)
-    if 'entries' in info and len(info['entries']) > 0:
-        info = info['entries'][0]
-        file_path = ydl.prepare_filename(info).replace(".webm", ".mp3").replace(".m4a", ".mp3")
-        msg = await ABH.send_file(
+        info = await asyncio.to_thread(ydl.extract_info, f"ytsearch:{query}", download=False)
+        if 'entries' in info and len(info['entries']) > 0:
+            video_info = info['entries'][0]
+            video_id = video_info.get('id')
+            if video_id in audio_cache:
+                val = audio_cache[video_id]
+                if isinstance(val, dict):
+                    await ABH.send_file(
+                        1910015590,
+                        file=val["file_id"],
+                        caption=f"{x}",
+                        attributes=[
+                            DocumentAttributeAudio(
+                                duration=val.get("duration", 0),
+                                title=val.get("title"),
+                                performer='ANYMOUS'
+                            )
+                        ]
+                    )
+                    x += 1
+                    return
+        info = await asyncio.to_thread(ydl.extract_info, f"ytsearch:{query}", download=True)
+        if 'entries' in info and len(info['entries']) > 0:
+            info = info['entries'][0]
+            file_path = ydl.prepare_filename(info).replace(".webm", ".mp3").replace(".m4a", ".mp3")
+            msg = await ABH.send_file(
+                1910015590,
+                file=file_path,
+                caption=f"{x}",
+                attributes=[
+                    DocumentAttributeAudio(
+                        duration=info.get("duration", 0),
+                        title=info.get('title'),
+                        performer='ANYMOUS'
+                    )
+                ]
+            )
+            audio_cache[info.get("id")] = {
+                "file_id": msg.file.id,
+                "title": info.get("title"),
+                "duration": info.get("duration", 0),
+                "query": query
+            }
+            save_cache()
+            x += 1
+    except Exception as e:
+        ABH.send_message(
             1910015590,
-            file=file_path,
-            caption=f"{x}",
-            attributes=[
-                DocumentAttributeAudio(
-                    duration=info.get("duration", 0),
-                    title=info.get('title'),
-                    performer='ANYMOUS'
-                )
-            ]
+            e
         )
-        audio_cache[info.get("id")] = {
-            "file_id": msg.file.id,
-            "title": info.get("title"),
-            "duration": info.get("duration", 0),
-            "query": query
-        }
-        save_cache()
-        x += 1
 ABH.run_until_disconnected()
