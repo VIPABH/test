68ea0541c3966b8cea007d1cab1cde0dd81348e1
VIPABH <a891983.2@gmail.com>
2025-06-09 02:50:50 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,7 +1,7 @@
 from ABH import ABH, events, bot_token
 from datetime import datetime
 import os, json, pytz
-from code import *)
+from code import *
 def main():
     print("config is starting...")
     ABH.start(bot_token=bot_token)
