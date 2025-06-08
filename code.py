dab7c99765ecd8dbd47a9bf85e28a97d91efdf81
VIPABH <a891983.2@gmail.com>
2025-06-09 02:56:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,14 @@
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
 from telethon import events
 from ABH import ABH
-@client.on(events.NewMessage(pattern='^امسح$'))
+@ABH.on(events.NewMessage(pattern='^امسح$'))
 async def delete_all_media(event):
     chat = event.chat_id
     deleted_count = 0
-    async for msg in client.iter_messages(chat):
+    async for msg in ABH.iter_messages(chat):
         if msg.media:
             try:
-                await client.delete_messages(chat, msg.id)
+                await ABH.delete_messages(chat, msg.id)
                 deleted_count += 1
             except Exception:
                 pass
