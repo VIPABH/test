366554ed307d4f4a7798d3c32ad3fdc0a318fa7d
VIPABH <a891983.2@gmail.com>
2025-06-09 02:04:52 +0300

    Create ABH.py

---
+++ ABH.py
@@ -0,0 +1,6 @@
+from telethon import TelegramClient, events
+import redis, os
+api_id = int(os.environ.get('API_ID'))
+api_hash = os.environ.get('API_HASH')
+bot_token = os.environ.get('BOT_TOKEN')
+ABH = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
