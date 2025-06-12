82a88517286313ae838a608b4538e89eeb55a659
VIPABH <a891983.2@gmail.com>
2025-06-12 11:07:40 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,9 +3,9 @@ from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantCreat
 from telethon import events
 from ABH import ABH
 
-@ABH.on(events.NewMessage)
+@ABH.on(events.NewMessage(pattern='ها'))
 async def x(event):
     await event.respond("الرجاء إرسال معرف القناة:")
-    response = await client.wait_for(events.NewMessage(from_user=event.sender_id))
+    response = await ABH.wait_for(events.NewMessage(from_user=event.sender_id))
     chanel = response.raw_text
     await event.reply(f'chanel={chanel}')
