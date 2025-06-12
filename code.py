bca09d2d241b307bf739c0db7fe38fe28c99ec39
VIPABH <a891983.2@gmail.com>
2025-06-12 11:08:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 from telethon import events
 from telethon.tl.custom import Button
-
+from ABH import *
 @ABH.on(events.NewMessage(pattern='/start'))
 async def x(event):
     async with ABH.conversation(event.chat_id) as conv:
