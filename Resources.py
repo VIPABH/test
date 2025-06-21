2aa2d1e106c37f4ca34e4eb095c077791b766d2e
VIPABH <a891983.2@gmail.com>
2025-06-21 23:42:32 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -1,5 +1,9 @@
 from telethon import events
-async def mention(event, sender):
+async def ment(sender):
         name = sender.first_name or 'name'
         id = sender.id
+        return f"[{name}](tg://user?id={id})"
+async def mention(event):
+        name = event.first_name or 'name'
+        id = event.id
         return f"[{name}](tg://user?id={id})"
