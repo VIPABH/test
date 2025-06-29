f4e7686c2f48e5d5d34e599a060e1f5b911a0b8a
VIPABH <a891983.2@gmail.com>
2025-06-29 10:57:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,19 @@
 from telethon import events
 from ABH import ABH
-import os, asyncio
-@ABH.on(events.NewMessage)
-async def mypic(event):
-    sender = await event.get_sender()
-    user = await event.client.get_entity(sender.id)
-    print(sender)
-    await asyncio.sleep(4)
-    print(user)
+
+@ABH.on(events.NewMessage(pattern='^نبذتي$'))  # الأمر الذي يستقبله البوت
+async def get_bio(event):
+    try:
+        # جلب معلومات المستخدم
+        user = await event.client.get_entity(event.sender_id)
+        
+        # التحقق من وجود نبذة
+        bio = getattr(user, 'bio', None)  # إذا لم توجد نبذة، يرجع None
+        
+        if bio:
+            await event.reply(f"نبذتك: \n`{bio}`")
+        else:
+            await event.reply("⚠️ لا يوجد لديك نبذة في الملف الشخصي!")
+            
+    except Exception as e:
+        await event.reply(f"❌ حدث خطأ: {e}")
