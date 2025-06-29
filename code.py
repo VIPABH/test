d599a4c493be7cafbd22e6b2781d28ad1579b232
VIPABH <a891983.2@gmail.com>
2025-06-29 10:08:25 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,11 @@ from telethon import events
 from ABH import ABH
 @ABH.on(events.NewMessage('^صورتي$'))
 async def mypic(event):
-    s = await event.get_sender()
-    photo = await event.client.download_profile_photo(
-    InputPeerUser(user_id=s.id, access_hash=s.access_hash),
-    file=f"user_{s.id}.jpg")
-    await ABH.send_file(event.chat_id, file=photo, reply_to=event.id)
+    try:
+        s = await event.get_sender()
+        photo = await event.client.download_profile_photo(
+        InputPeerUser(user_id=s.id, access_hash=s.access_hash),
+        file=f"user_{s.id}.jpg")
+        await ABH.send_file(event.chat_id, file=photo, reply_to=event.id)
+    except Exception as e:
+        await event.reply(e)
