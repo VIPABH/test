caa1f05d95980c899fa0d1885efc2b17cea59399
VIPABH <a891983.2@gmail.com>
2025-06-29 11:05:25 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,13 @@ from telethon import events
 from ABH import ABH
 @ABH.on(events.NewMessage)
 async def get_bio(event):
+    user = await event.client.get_entity(event.sender_id)
+    if user.photo:
+        avatar_path = await event.client.download_profile_photo(
+            user,
+            file=f"temp/avatar_{user.id}.jpg"
+        )
     FullUser = (await event.client(GetFullUserRequest(event.sender_id))).full_user
     bio = FullUser.about
     bio_text = f"\n{bio}" if bio and bio.strip() else ""
-    await event.reply(bio_text)
+    await ABH.send_file(event.chat_id, file= avatar_path, caption=bio_text,reply_to=event.id)
