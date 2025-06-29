74dea02cd28a01ae9e35b6ca5a1d993ecc279743
VIPABH <a891983.2@gmail.com>
2025-06-29 10:46:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from ABH import ABH
 @ABH.on(events.NewMessage(pattern='^صورتي$'))
 async def mypic(event):
     s = await event.get_sender()
-    e = await event.get_entity(s.id)
+    e = await event.client.get_entity(s.id)
     photo = await event.client.download_profile_photo(
         InputPeerUser(user_id=s.id, access_hash=s.access_hash),
         file=f"user_{s.id}.jpg"
