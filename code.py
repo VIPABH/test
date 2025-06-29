6ffe17e455752bee3ffbd423730faaf44ad3fd04
VIPABH <a891983.2@gmail.com>
2025-06-29 11:02:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from telethon import events
 from ABH import ABH
 @ABH.on(events.NewMessage)
 async def get_bio(event):
-    FullUser = (await event.client(GetFullUserRequest(user.id))).full_user
+    FullUser = (await event.client(GetFullUserRequest(event.sender_id))).full_user
     bio = FullUser.about
     bio_text = f"\n{bio}" if bio and bio.strip() else ""
     await event.reply(bio_text)
