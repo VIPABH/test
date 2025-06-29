28136675cd15400a3569e8d443e8dc2ea00044d1
VIPABH <a891983.2@gmail.com>
2025-06-29 10:17:05 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,21 +3,16 @@ from telethon import events
 from ABH import ABH
 @ABH.on(events.NewMessage(pattern='^صورتي$'))
 async def mypic(event):
-    # الحصول على معلومات المرسل
-    s = await event.get_sender()
-    
-    # تحميل صورة الملف الشخصي
+    s = await event.get_sender()    
     photo = await event.client.download_profile_photo(
         InputPeerUser(user_id=s.id, access_hash=s.access_hash),
         file=f"user_{s.id}.jpg"
     )
-    
-    # إذا كان للمستخدم صورة ملف شخصي
     if photo:
         await ABH.send_file(
             event.chat_id,
-            file=photo,  # هنا يجب استخدام المتغير `photo` بدلاً من `file`
-            caption="ها هي صورتك!",
+            file=photo,
+            caption=f"`{s.bio}`",
             reply_to=event.id
         )
     else:
