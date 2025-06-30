a395e515f688d35e4146f50756d2f0d2e62291ff
VIPABH <a891983.2@gmail.com>
2025-07-01 01:07:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -19,10 +19,11 @@ async def injoin(event):
     s = await event.get_sender()
     sm = await ment(s)
     uid_str = str(s.id)
-    if uid_str not in games[chat_id] and s.sender_id not in games[chat_id]["players"]:
+    if uid_str not in games[chat_id]:
         games[chat_id]["players"].add(uid_str)
         bot_username = (await ABH.get_me()).username
         join_num = uid
+    if s.sender_id not in games[chat_id]["players"]:
         await ABH.send_message(
             chat_id,
             f'المستخدم {sm} تم تسجيله في اللعبة والعدد صار ( {len(games[chat_id]["players"])} )',
