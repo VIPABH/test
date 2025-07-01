6c06ff544a12a1579e434800252d004ab8292f02
VIPABH <a891983.2@gmail.com>
2025-07-01 10:11:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,19 @@
+from telethon.errors import MessageIdInvalidError
 from telethon import events
 from ABH import ABH
 @ABH.on(events.NewMessage)
 async def scan(event):
-    x = 'x04ou'
-    for i in range(385):
-        m = f'https://t.me/{x}/{i}'
-        s = await ABH.get_messages(x, ids=i)
-        if s and not s.media:
-            await event.reply(s)
+    channel_id = 'x04ou'
+    for i in range(1, 386):
+        try:
+            msg = await ABH.get_messages(channel_id, ids=i)
+            if msg and not msg.media:
+                if msg.message:
+                    await event.reply(f"رسالة [{i}]:\n{msg.message}")
+                else:
+                    await event.reply(f"رسالة [{i}] موجودة ولكن لا تحتوي على نص.")
+        except MessageIdInvalidError:
+            continue
+        except Exception as e:
+            await event.reply(f"حدث خطأ في الرسالة {i}:\n{str(e)}")
+            continue
