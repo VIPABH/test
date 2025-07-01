05d7d087f2690c3d664e6ee6b95f03f20ff52606
VIPABH <a891983.2@gmail.com>
2025-07-01 10:21:37 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ from telethon import events
 from ABH import ABH
 from telethon.tl.types import DocumentAttributeAudio
 import asyncio
+x = []
+z = []
 @ABH.on(events.NewMessage)
 async def scan(event):
     channel = 'x04ou'
@@ -10,12 +12,17 @@ async def scan(event):
             msg = await ABH.get_messages(channel, ids=i)
             if not msg:
                 await event.reply(f'الرساله غير موجوده {i}')
+                x.append(i)
             if not msg.media or not msg.document:
                 await event.reply(f'الرساله لا تحتوي علئ ملف {i}')
+                x.append(i)
                 continue
             for attr in msg.document.attributes:
                 if isinstance(attr, DocumentAttributeAudio) and not attr.voice:
+                    z.append(i)
                     break
             await asyncio.sleep(0.3)
         except Exception:
-            continue 
+            continue
+        await event.reply(f'{x}')
+        await event.reply(f'{z}')
