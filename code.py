84afcfa22b304d35e39b2739f6cd9f8e8819997e
VIPABH <a891983.2@gmail.com>
2025-07-04 13:26:52 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ from ABH import *
 from telethon.tl.types import InputDocument
 @ABH.on(events.NewMessage)
 async def x(event):
+    doc = event.message.media.document
     input_doc = InputDocument(
         id=doc.id,
         access_hash=doc.access_hash,
