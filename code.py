8e31abe7a57a93661ea12623e1a966cd6da4351d
VIPABH <a891983.2@gmail.com>
2025-07-04 13:18:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -15,4 +15,4 @@ async def x(event):
         if not file_id:
             await event.reply('ماكو ايدي')
             return
-        await event.reply(str(file_id)
+        await event.reply(str(file_id))
