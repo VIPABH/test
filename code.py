f9fd071dacd69929c8b4b7428be1f3f9d54ee211
VIPABH <a891983.2@gmail.com>
2025-07-04 13:23:17 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,4 +2,5 @@ from ABH import *
 @ABH.on(events.NewMessage)
 async def x(event):
     msg = event.message
+    await event.reply(f'{msg.id}')
     await ABH.send_file(event.chat_id, file=msg.id, reply_to=event.id)
