c89853c27826c618f1544433ac8570fa6e4a2371
VIPABH <a891983.2@gmail.com>
2025-07-04 13:22:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,14 +2,4 @@ from ABH import *
 @ABH.on(events.NewMessage)
 async def x(event):
     msg = event.message
-    if msg.media:
-        file_id = None
-        if msg.photo:
-            file_id = msg.photo.id
-        elif msg.document:
-            file_id = msg.document.id
-        elif msg.video:
-            file_id = msg.video.id
-        else:
-            file_id = None
-    await ABH.send_file(event.chat_id, file=file_id, reply_to=event.id)
+    await ABH.send_file(event.chat_id, file=msg.id, reply_to=event.id)
