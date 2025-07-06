b65b54dde77af84d4a4c13714c8b8923b9b5e77d
VIPABH <a891983.2@gmail.com>
2025-07-06 22:44:18 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,9 +2,11 @@ from telethon.tl.types import InputDocument
 from Resources import mention, hint, wfffp
 # from other import botuse, is_assistant
 from telethon import Button, events
-from Program import chs
+# from Program import chs
 import random, redis
 from ABH import ABH
+async def chs(e, t):
+    ABH.send_message(e.chat_id, t, reply_to=e.id)
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 session = {}
 banned = ['وضع ردي', 'وضع رد', 'وضع رد مميز', 'الغاء', 'حذف رد', 'حذف الردود', 'عرض الردود', 'حذف ردي']
