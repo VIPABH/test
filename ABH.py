b600744457653058cd2851a2cf187aa27178615b
VIPABH <a891983.2@gmail.com>
2025-07-22 13:42:42 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,12 +1,13 @@
 from telethon import TelegramClient, events
-import redis, os
+# import redis, os
+import os
 api_id = int(os.environ.get('API_ID'))
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 ABH = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
-r = redis.StrictRedis(
-    host='localhost',
-    port=6379,
-    db=0,
-    decode_responses=True
-)
+# r = redis.StrictRedis(
+#     host='localhost',
+#     port=6379,
+#     db=0,
+#     decode_responses=True
+# )
