7c79d1d79acad3cbfefe0ef5a149db2d47f7af87
VIPABH <a891983.2@gmail.com>
2025-07-29 12:13:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import SendReactionRequest
 from telethon.tl.types import InputPeerUser, ReactionEmoji
 
-@ABH.on(events.NewMessage('ها'))
+@ABH.on(events.NewMessage(pattern='ها'))
 async def react_to_message(event):
     await ABH(SendReactionRequest(
         peer=event.chat_id,
