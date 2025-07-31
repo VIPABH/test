6459ea5a6afa542f7ff60c6298f5f50e1fe03c30
VIPABH <a891983.2@gmail.com>
2025-07-31 13:36:12 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 from telethon import events, Button
 
-@bot.on(events.NewMessage(pattern='/start'))
+@ABH.on(events.NewMessage(pattern='/start'))
 async def start(event):
     await event.respond(
         'اضغط على الزر لعرض إشعار:',
         buttons=[Button.inline('عرض إشعار', b'show_alert')]
     )
 
-@bot.on(events.CallbackQuery(data=b'show_alert'))
+@ABH.on(events.CallbackQuery(data=b'show_alert'))
 async def callback(event):
     await event.answer("🚨 هذا هو الإشعار الذي طلبته", alert=True)
