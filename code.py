2a62470fc0af4c414e4fee31cecbb7e541e44f6e
VIPABH <a891983.2@gmail.com>
2025-08-03 01:36:55 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
 from telethon import events, Button
-from ABH import ABH
-@ABH.on(events.NewMessage(pattern=r'^ازعاج (\d+)$'))
-async def spam(event):
-    await event.reply("تم التعرف على الأمر!")
+from ABH import ABH as client
+@client.on(events.NewMessage(pattern="/ابدأ"))
+async def handler(event):
+    user_id = event.sender_id
+    chat_id = event.chat_id
+
+    async def wait_reply(new_event):
+        if new_event.chat_id == chat_id and new_event.sender_id == user_id:
+            await new_event.reply(f"تم استلام ردك: {new_event.text}")
+            client.remove_event_handler(wait_reply, events.NewMessage)
+
+    client.add_event_handler(wait_reply, events.NewMessage)
+    await event.reply("ارسل ردك الآن...")
