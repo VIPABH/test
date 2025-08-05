424237d3c3b22dd725d030bd3a38000ea085a23f
VIPABH <a891983.2@gmail.com>
2025-08-05 03:21:49 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,16 +8,18 @@ x_ar = {
 }
 @ABH.on(events.NewMessage)
 async def xss(e):
-    emoji, country = random.choice(list(x_ar.items()))
-    g = e.chat_id
-    id = e.sender_id
-    t = e.text
-    if g not in session or id not in session:
-        session[id] = emoji, country
-    if t == 'h':
-        await e.reply(f'ما هو اسم العلم {emoji}')
-    em = e.text
-    country = session[id]
-    if em == country:
-        await e.reply('احسنت')
-
+    try:
+        emoji, country = random.choice(list(x_ar.items()))
+        g = e.chat_id
+        id = e.sender_id
+        t = e.text
+        if g not in session or id not in session:
+            session[id] = emoji, country
+        if t == 'h':
+            await e.reply(f'ما هو اسم العلم {emoji}')
+        em = e.text
+        country = session[id]
+        if em == country:
+            await e.reply('احسنت')
+    except Exception as e:
+        await e.reply(str(e))
