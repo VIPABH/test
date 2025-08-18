a7b4daf6d5f458c086498640512d798ef2cd3fac
VIPABH <a891983.2@gmail.com>
2025-08-18 20:25:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,35 +1,15 @@
 from telethon import events
-from ABH import ABH  # استيراد العميل باسم ABH
-
-target_file_id = None  # نخزن هنا الـ file_id للمتحرك
-
-# أمر لتعيين الـ file_id عبر الرد
-@ABH.on(events.NewMessage(pattern=r"^/ض$"))
-async def set_file_id(event):
-    global target_file_id
-    if event.is_reply:
-        reply_msg = await event.get_reply_message()
-        if reply_msg.document:
-            target_file_id = reply_msg.file.id
-            await event.reply("✅ تم حفظ المتحرك.")
-        else:
-            await event.reply("❌ الرد يجب أن يكون على متحرك.")
-    else:
-        await event.reply("❌ يجب الرد على المتحرك.")
-
-# مراقبة الرسائل وحذف أي رسالة بنفس الـ file_id
+from ABH import ABH
+import os, json
+def file(filename, data):
+    if not os.path.exists(filename):
+        os.mkdir(filename)
+        with open(filename, 'r', encoding='utf-8') as f:
+            f.write(f'{data}, \n')
 @ABH.on(events.NewMessage)
-async def delete_matching(event):
-    global target_file_id
-    if target_file_id and event.document:
-        if event.file.id == target_file_id:
-            # حذف الرسالة الحالية
-            await event.delete()
-
-            # إذا كانت هذه الرسالة رد على رسالة أخرى، نحذف الرسالة الأصلية أيضًا
-            reply_msg = await event.get_reply_message()
-            if reply_msg:
-                await reply_msg.delete()
-
-print("Bot is running...")
-ABH.run_until_disconnected()
+async def all(e):
+    t = e.text
+    if t == 'مل':
+        r = await e.get_reply_message()
+        file(r.text, 'test')
+        return
