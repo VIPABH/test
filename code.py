852e41c5c5b9ed1ae4b314e97aefa417e17bd061
VIPABH <a891983.2@gmail.com>
2025-08-19 00:10:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,23 +2,20 @@ from telethon import events
 from ABH import ABH
 import os, json
 
-def file(filename, data):
-    # إذا الملف ما موجود، ننشئه
+def file(filename='data.txt', data):
     if not os.path.exists(filename):
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(f'{data}, \n')
     else:
-        # إذا الملف موجود، نضيف البيانات بدون حذف القديم
         with open(filename, 'a', encoding='utf-8') as f:
             f.write(f'{data}, \n')
-
 @ABH.on(events.NewMessage)
 async def all(e):
     t = e.text
     if t == 'مل':
         r = await e.get_reply_message()
         if r:  # نتأكد أن الرسالة رد
-            file("data.txt", r.text)  # نخزن الرد داخل ملف data.txt
+            file("data.txt", r)  # نخزن الرد داخل ملف data.txt
             await e.reply("تم الحفظ ✅")
         else:
             await e.reply("رد على رسالة حتى أحفظها ❌")
