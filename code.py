684c8cefd7feb674cc1fa2d736198f126b0841e7
VIPABH <a891983.2@gmail.com>
2025-08-26 12:00:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,18 +4,23 @@ import os, json
 
 def save(data, filename='data.txt'):
     if not os.path.exists(filename):
-        with open(filename, 'w', encoding='utf-8') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             f.write(f'{data}, \n')
+            load = f.read()
+            return load
     else:
-        with open(filename, 'a', encoding='utf-8') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             f.write(f'{data}, \n')
+            load = f.read()
+            return load
 @ABH.on(events.NewMessage)
 async def all(e):
     t = e.text
     if t == 'مل':
         r = await e.get_reply_message()
         if r:
-            save(r.text, filename='data.txt')
+            x = save(r.text, filename='data.txt')
             await e.reply("تم الحفظ ✅")
+            await e.reply(x)
         else:
             await e.reply("رد على رسالة حتى أحفظها ❌")
