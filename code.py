9ce7a24fe51bff5d3d4808073ca81ec85ca167de
VIPABH <a891983.2@gmail.com>
2025-08-26 12:27:18 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,20 +2,17 @@ from telethon import events
 from ABH import ABH
 import os
 
-def save(data, filename='data.txt'):
-    # إذا الملف موجود نفتح بإضافة + قراءة
-    # إذا ما موجود، نفتح بكتابة + قراءة
+def save(data, filename='d.json'):
     mode = 'a+' if os.path.exists(filename) else 'w+'
     with open(filename, mode, encoding='utf-8') as f:
         f.write(f'{data}, \n')
-        f.seek(0)  # نرجع المؤشر للبداية حتى نكدر نقرا
+        f.seek(0)
         return f.read()
-
 @ABH.on(events.NewMessage)
 async def all(e):
     if e.text == 'مل':
         r = await e.get_reply_message()
-        if r and r.text:
+        if r and r.sender_id:
             x = save(r.text, filename='data.txt')
             await e.reply("تم الحفظ ✅")
             await e.reply(x)
