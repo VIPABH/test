2912de7be4e5e27d8ec6793b630bcc33b8658a25
VIPABH <a891983.2@gmail.com>
2025-08-26 11:57:49 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,21 @@
 from telethon import events
 from ABH import ABH
 import os, json
-def save(file, data):
-    if not os.path.exists(file):
-        with open(file, "w") as f:
-            pass
-    if not data or data is None:
-        load = json.load(open(os.path.join(file)))
-        return load
-    with open(os.path.join(file), "w") as f:
-        json.dump(data, f)
-        load = json.load(open(os.path.join(file)))
-        return load
-@ABH.on(events.NewMessage(pattern=r"تخزين"))
-async def handler(event):
-    r = await event.get_reply_message()
-    if r:
-        data = r.text
-        s = save('t.json', data)
-        await event.reply("تم تخزين الرسالة.")
-        await event.reply(s)
+
+def save(data, filename='data.txt'):
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(f'{data}, \n')
+    else:
+        with open(filename, 'a', encoding='utf-8') as f:
+            f.write(f'{data}, \n')
+@ABH.on(events.NewMessage)
+async def all(e):
+    t = e.text
+    if t == 'مل':
+        r = await e.get_reply_message()
+        if r:
+            save(r.text, filename='data.txt')
+            await e.reply("تم الحفظ ✅")
+        else:
+            await e.reply("رد على رسالة حتى أحفظها ❌")
