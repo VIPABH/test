972858feda59991731b28563a6dc55db18d9d48d
VIPABH <a891983.2@gmail.com>
2025-09-08 00:30:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,11 @@
-from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events
-from ABH import ABH
+from ABH import *
 @ABH.on(events.NewMessage)
-async def get_bio(event):
-    user = await event.client.get_entity(event.sender_id)
-    if user.photo:
-        avatar_path = await event.client.download_profile_photo(
-            user,
-            file=f"temp/avatar_{user.id}.jpg"
-        )
-    FullUser = (await event.client(GetFullUserRequest(event.sender_id))).full_user
-    bio = FullUser.about
-    bio_text = f"\n{bio}" if bio and bio.strip() else ""
-    await ABH.send_file(event.chat_id, file= avatar_path, caption=bio_text,reply_to=event.id)
+async def mx(event):
+    for i in range(385, 432):
+        x = await ABH.get_message("x04ou", i)
+        if x:
+            msg = f'{i} موجود'
+        else:
+            msg = f'{i} غير موجود'
+    await event.reply(msg)
