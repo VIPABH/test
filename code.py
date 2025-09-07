74f33603d07c3ee2e4e984601875dca05d67efe2
VIPABH <a891983.2@gmail.com>
2025-09-08 00:31:06 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from ABH import *
 @ABH.on(events.NewMessage)
 async def mx(event):
     for i in range(385, 432):
-        x = await ABH.get_message("x04ou", i)
+        x = await ABH.get_messages("x04ou", i)
         if x:
             msg = f'{i} موجود'
         else:
