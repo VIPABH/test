97faec93f4002dc1b6e5a16c6c8022b1d8993c89
VIPABH <a891983.2@gmail.com>
2025-09-19 00:57:45 +0300

    Update code.py

--- code.py
+++ code.py
@@ -106,7 +106,7 @@ async def track_messages(e):
     msg_type = get_message_type(m)
 
 
-    await e.reply(f"{user_stats}")
     user_stats = await info(e, msg_type)
+    await e.reply(f"{user_stats}")
     stats_str = json.dumps(user_stats, ensure_ascii=False, indent=2)
     await e.reply(f"إحصائياتك حتى الآن:\n{stats_str}")
