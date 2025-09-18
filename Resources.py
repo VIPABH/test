667646698d85dc76b32f16e7b0a9dd33b5bd9180
VIPABH <a891983.2@gmail.com>
2025-09-19 01:42:44 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -11,38 +11,38 @@ def get_message_type(msg:Message)->str:
     if msg is None:
         return
     if msg.message and not msg.media:
-        return "text"
+        return "الرسائل"
     if isinstance(msg.media,MessageExtendedMediaPreview) or isinstance(msg.media,MessageExtendedMedia):
         inner=msg.media.media
         return get_message_type(Message(id=msg.id,media=inner))
     if isinstance(msg.media,MessageMediaPhoto):
-        return "photo"
+        return "الصور"
     if isinstance(msg.media,MessageMediaDocument):
         for attr in msg.media.document.attributes:
             if isinstance(attr,DocumentAttributeAnimated):
-                return "gif"
+                return "الgif"
         for attr in msg.media.document.attributes:
             if isinstance(attr,DocumentAttributeVideo):
-                if getattr(attr,"round_message",False)
-                    return "voice note"
-                return "video"
+                if getattr(attr,"round_message",False):
+                    return "الفويس نوت"
+                return "الفيديو"
         for attr in msg.media.document.attributes:
             if isinstance(attr,DocumentAttributeSticker):
-                return "sticker"
+                return "الستيكرات"
             if isinstance(attr,DocumentAttributeAudio):
-                return "voice" if getattr(attr,"voice",False) else "audio"
+                return "الفويسات" if getattr(attr,"voice",False) else "الصوتيات"
         mime=msg.media.document.mime_type or ""
         if mime.startswith("image/"):
-            return "image"
+            return "الصور"
         elif mime.startswith("video/"):
-            return "video"
+            return "الفيديوهات"
         elif mime.startswith("audio/"):
-            return "audio"
-        return "document"
+            return "الصوتيات"
+        return "الملفات"
     if isinstance(msg.media,MessageMediaGeo):
-        return "location"
+        return "المواقع"
     if isinstance(msg.media,MessageMediaPoll):
-        return "poll"
+        return "الاستفتاءات"
     return
 @ABH.on(events.NewMessage)
 async def track_messages(e):
