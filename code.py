070e1f6689dcc04cf7834a8f182a196a44c9de6a
VIPABH <a891983.2@gmail.com>
2025-10-31 17:08:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
-from ABH import ABH
 import asyncio
+from ABH import ABH
+
 async def x():
     await ABH.send_message(1910015590, ".")
     await asyncio.sleep(10)
-async def s():
-    while True:  
+
+async def main():
+    while True:
         await x()
+
 if __name__ == "__main__":
-    asyncio.run(s())
+    ABH.loop.run_until_complete(main())
