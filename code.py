5685e87daf2e76e0da73abe569c7c1b075d6435a
VIPABH <a891983.2@gmail.com>
2025-10-31 17:04:20 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import asyncio
 async def x():
     await ABH.send_message(1910015590, ".")
     await asyncio.sleep(10)
-def s():
+async def s():
 if __name__ == "__main__":
     await x()
-    s()
+    await s()
