5d2804f32d0fbdd3aa33306b824f016482325dbe
VIPABH <a891983.2@gmail.com>
2025-11-07 23:14:46 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,8 @@
 from telethon import events
-
-last_event_id = None  # متغير عالمي لتخزين آخر حدث تم التعامل معه
-
+from ABH import *
 @ABH.on(events.ChatAction)
 async def on_chat_action(event):
-    global last_event_id
-    me = await ABH.get_me()
-    
-    # تحقق من التكرار
-    if event.id == last_event_id:
-        return  # تجاهل الحدث إذا سبق التعامل معه
-    last_event_id = event.id
-
-    # تحقق من إضافة الحساب نفسه فقط
-    if event.user_added and event.user_id == me.id:
-        await event.reply('تمت إضافة الحساب نفسه')
+    if event.user_joined and event.user_added:
+        await event.reply("دخول")
+    elif event.user_added:
+        await event.reply("إضافة")
