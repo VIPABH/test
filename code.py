e3ae442b1d4cac2350bada8139f903eea0da1798
VIPABH <a891983.2@gmail.com>
2025-11-07 23:22:17 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,7 @@ from ABH import *
 import asyncio
 @ABH.on(events.ChatAction)
 async def on_chat_action(event):
+    await event.reply(str(event))
     me = await ABH.get_me()
     if event.user_added and event.user_id == me.id:
         await event.reply("إضافة")
-        await asyncio.sleep(2)
-        await event.reply(str(event))
