bd39f94cd9ed3c6aca04411af3fdbab85f975d80
VIPABH <a891983.2@gmail.com>
2025-11-07 23:06:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,5 +2,5 @@ from telethon import events
 from ABH import *
 @ABH.on(events.ChatAction)
 async def on_chat_action(event):
-    if event.user_added and event.sender_id == await ABH.get_me().id:
+    if event.user_added and event.user_id == await ABH.get_me().id:
         await event.reply('...')
