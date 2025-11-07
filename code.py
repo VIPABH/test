8d0bb23f01748cdc6b24272144e2204918e8bed7
VIPABH <a891983.2@gmail.com>
2025-11-07 23:44:05 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
-from telethon import TelegramClient, events
+from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.types import ChatAdminRights
+from telethon import TelegramClient, events
 from ABH import ABH as bot
 @bot.on(events.ChatAction)
 async def check_admin(event):
@@ -11,6 +12,6 @@ async def check_admin(event):
                 await event.reply("تم التأكد: البوت مشرف ✅")
             else:
                 await event.reply("البوت ليس مشرف! سأخرج ❌")
-                await bot.kick_participant(event.chat_id, me.id)
+                await ABH(LeaveChannelRequest(event.chat_id))
         except Exception as e:
             print(f"خطأ عند التحقق من الصلاحيات: {e}")
