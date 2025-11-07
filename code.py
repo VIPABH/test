4308b0077d8ebbbfde9f625e289d1719119a4fb5
VIPABH <a891983.2@gmail.com>
2025-11-07 23:00:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,15 +2,5 @@ from telethon import events
 from ABH import *
 @ABH.on(events.ChatAction)
 async def on_chat_action(event):
-    if event.user_joined:
-        await event.reply("دخول")
-    elif event.user_added:
-        await event.reply("إضافة")
-    elif event.user_left:
-        await event.reply("مغادرة")
-    elif getattr(event, "kicked_by", None):
-        await event.reply("طرد")
-    elif getattr(event, "new_admin_rights", None):
-        await event.reply("رفع")
-    elif getattr(event, "banned_rights", None):
-        await event.reply("تقييد")
+    print(event)
+    await event.reply(str(event))
