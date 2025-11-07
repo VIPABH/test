b57f2db02056dfd1438125b675385596e2e30d63
VIPABH <a891983.2@gmail.com>
2025-11-07 23:19:14 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 from telethon import events
 from ABH import *
+import asyncio
 @ABH.on(events.ChatAction)
 async def on_chat_action(event):
     me = await ABH.get_me()
     if event.user_added and event.user_id == me.id:
         await event.reply("إضافة")
+        await asyncio.sleep(2)
+        await event.reply(str(event))
