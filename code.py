e0cb86c2bde328d0cec60f1a9b0a0327cc68723e
VIPABH <a891983.2@gmail.com>
2025-11-08 18:49:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,22 +10,26 @@ async def monitor_everything(event):
         user_id = getattr(event, "user_id", getattr(getattr(event, "participant", None), "user_id", None))
         if not user_id == me.id:
             return
+        update = getattr(event, "update", event)
         channel_id = getattr(event, "channel_id", None)
         participant = getattr(event, "participant", None)
-        if isinstance(participant, (types.ChannelParticipant, types.ChannelParticipantAdmin)):
-            if isinstance(participant, (types.ChannelParticipantLeft, types.ChannelParticipantBanned)):
-                entity = await ABH.get_entity(channel_id)
-                await ABH.send_message(entity, "⚠️ عذرًا، تم تنزيل البوت أو طرده، سأغادر القناة.")
-                await asyncio.sleep(1)
-                await ABH(LeaveChannelRequest(channel_id))
-                return
-        print(type(event))
+        if not isinstance(participant, (types.ChannelParticipant, types.ChannelParticipantAdmin)):
+            return
+        if isinstance(participant, (types.ChannelParticipantLeft, types.ChannelParticipantBanned)):
+            entity = await ABH.get_entity(channel_id)
+            await ABH.send_message(entity, "😢")
+            await asyncio.sleep(1)
+            await ABH(LeaveChannelRequest(channel_id))
+            return
+        actor_id = getattr(update, "actor_id", None) or getattr(update, "user_id", None)
+        actor = await ABH.get_entity(actor_id)
+        mention = f"[{actor.first_name}](tg://user?id={actor.id})"
         entity = await ABH.get_entity(channel_id)
         perms = await ABH.get_permissions(channel_id, me.id)
         if perms.is_admin:
-            await ABH.send_message(entity, f"✅ اشكرك على الإضافة ")
+            await ABH.send_message(entity, f"اشكرك على الاضافة وردة {mention}")
         else:
-            await ABH.send_message(entity, "⚠️ عذرًا، لا أستطيع البقاء هنا إلا إذا كنت مشرفًا.")
+            await ABH.send_message(entity, "خلي ييدوكم يله بيباي")
             await asyncio.sleep(1)
             await ABH(LeaveChannelRequest(channel_id))
     except:
