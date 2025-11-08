b5a80d70442ffeb343ffa6779936d5565a6bb68c
VIPABH <a891983.2@gmail.com>
2025-11-08 09:31:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,27 +2,25 @@ from telethon import events
 from telethon.tl.types import UpdateChannelParticipant
 from telethon.tl.functions.channels import LeaveChannelRequest
 from ABH import ABH as bot
-import asyncio
+from Resources import *
 @bot.on(events.Raw)
 async def monitor_everything(event):
     try:
         print("📦 نوع التحديث:", type(event))
         if isinstance(event, UpdateChannelParticipant):
-            print("⚙️ حدث يخص المشاركين أو المشرفين.")
             me = await bot.get_me()
             if event.user_id == me.id:
-                print("👀 التغيير يخص البوت نفسه.")
                 try:
                     perms = await bot.get_permissions(event.channel_id, me.id)
                     if perms.is_admin:
-                        print("✅ البوت لا يزال مشرف.")
+                        await event.reply(f"اشكرك على الاضافه {await mention(event)}")
                     else:
-                        print("❌ تم تنزيل البوت من الإشراف! يغادر الآن...")
+                        await event.reply(f'عذرا بس ماكدر ابقه هنا الا ترفعني مشرف')
                         await bot(LeaveChannelRequest(event.channel_id))
                 except Exception as e:
                     print(f"⚠️ خطأ أثناء التحقق من الصلاحيات: {e}")
-        else:
-            print("🧩 نوع تحديث آخر غير متعلق بالمشاركين.")
-            print(event)
+        # else:
+        #     print("🧩 نوع تحديث آخر غير متعلق بالمشاركين.")
+        #     print(event)
     except Exception as e:
         print(f"🚨 حدث خطأ أثناء معالجة التحديث: {e}")
