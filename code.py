d3cf707ca6866733c17b57622f49e62d7127c708
VIPABH <a891983.2@gmail.com>
2025-11-08 15:08:13 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,34 +4,22 @@ from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.utils import get_display_name
 from Resources import *
 from ABH import ABH
-
+import asyncio
 @ABH.on(events.Raw)
 async def monitor_everything(event):
     me = await ABH.get_me()
     user_id = getattr(event, "user_id", getattr(getattr(event, "participant", None), "user_id", None))
     if not user_id == me.id:
         return
-    # if isinstance(event, UpdateChannelParticipant):
     channel_id = getattr(event, "channel_id", None)
-    #     participant = getattr(event, "participant", None)
-    #     if (participant is None or isinstance(participant, ChannelParticipantBanned)):
-    #         print(f"⚠️ البوت تم طرده أو حظره من القناة {channel_id}")
-    #         return
     try:
         entity = await ABH.get_entity(channel_id)
         perms = await ABH.get_permissions(channel_id, me.id)
         if perms.is_admin:
-            try:
-                user_entity = await ABH.get_entity(user_id)
-                user_name = get_display_name(user_entity)
-            except:
-                user_name = "صديقي"
-            await ABH.send_message(entity, f"✅ اشكرك على الإضافة {user_name}")
+            await ABH.send_message(entity, f"✅ اشكرك على الإضافة ")
         else:
             await ABH.send_message(entity, "⚠️ عذرًا، لا أستطيع البقاء هنا إلا إذا كنت مشرفًا.")
-            try:
-                await ABH(LeaveChannelRequest(channel_id))
-            except Exception as leave_err:
-                print(f"⚠️ خطأ أثناء مغادرة القناة: {leave_err}")
+            await asyncio.sleep(1)
+            await ABH(LeaveChannelRequest(channel_id))
     except Exception as e:
         print(f"⚠️ خطأ أثناء التحقق من الصلاحيات أو إرسال الرسائل: {e}")
