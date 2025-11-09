de898560be1ee37e5451472b79d7d63a768efcba
VIPABH <a891983.2@gmail.com>
2025-11-09 22:59:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ from telethon import events
 from Resources import *
 from ABH import ABH
 import asyncio
-@ABH.on(events.NewMessage)
+@ABH.on(events.Raw)
 async def get_invite_link(e):
     link = await ABH(ExportChatInviteRequest(e.chat_id))
     print("رابط الدعوة:", link.link)
