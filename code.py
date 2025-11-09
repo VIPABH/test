ae3f49bdfb1cfe9e67ace1f1b54f751542b97da1
VIPABH <a891983.2@gmail.com>
2025-11-09 23:00:08 +0300

    Update code.py

--- code.py
+++ code.py
@@ -7,5 +7,6 @@ from ABH import ABH
 import asyncio
 @ABH.on(events.Raw)
 async def get_invite_link(e):
-    link = await ABH(ExportChatInviteRequest(e.chat_id))
+    chat_id = getattr(event, "channel_id", None)
+    link = await ABH(ExportChatInviteRequest(chat_id))
     print("رابط الدعوة:", link.link)
