e6f2b5aa8c2a4d0e8c329b4fd363450e699d333a
VIPABH <a891983.2@gmail.com>
2025-11-19 23:19:12 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,15 @@
 from ABH import *
 commands = {}
-user_state = {}  # لتخزين الحالة لكل مستخدم
-
+user_state = {}
+async def x(e):
+    await e.reply('hi')
 @ABH.on(events.NewMessage)
 async def handle_commands(e):
     t = e.text
     uid = e.sender_id
-
+    if t == 'hi':
+        await x(e)
+        return
     if uid in user_state:
         state = user_state[uid]
         if state['step'] == 'await_old':
@@ -14,7 +17,6 @@ async def handle_commands(e):
             user_state[uid]['step'] = 'await_new'
             await e.reply('ارسل الامر المختصر')
             return
-
         elif state['step'] == 'await_new':
             old = state['old']
             new = t
@@ -23,18 +25,8 @@ async def handle_commands(e):
             else:
                 commands[old] = new
                 await e.reply(f'تم اضافه الامر: {old} -> {new}')
-            del user_state[uid]  # إنهاء الحالة
+            del user_state[uid]
             return
-    notf = []
-    if t == 'افحص':
-        for x in range(385, 491):
-            m = await ABH.get_messages('x04ou', ids=x)
-            if not m:
-                notf.append(x)
-        if notf:
-            await e.reply("الرسائل المفقودة:\n" + ", ".join(str(n) for n in notf))
-        else:
-            await e.reply("كل الرسائل موجودة")
     if t == 'ا':
         user_state[uid] = {'step': 'await_old'}
         await e.reply('يجري تغيير الامر, ارسل الامر الاساسي')
