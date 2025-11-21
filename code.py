f818c1271e3da3b92027638769cef7ee7097bc22
VIPABH <a891983.2@gmail.com>
2025-11-21 15:15:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,21 @@
 from ABH import *
 from telethon import events
+
+# تخزين آخر media group تمت معالجته
+processed_groups = set()
+
 @ABH.on(events.NewMessage)
 async def handle_commands(e):
-    print(e)
-    media = e.media
-    if media:
+    # إذا توجد وسائط
+    if e.media:
+        # إذا الرسالة جزء من media group
+        gid = getattr(e, 'grouped_id', None)
+
+        if gid:
+            # إذا سبق الرد على هذا الـ group → لا تكرر الرد
+            if gid in processed_groups:
+                return
+            processed_groups.add(gid)
+
+        # ردّ واحد فقط
         return await e.reply('x')
