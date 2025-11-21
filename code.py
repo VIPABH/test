c597989321d563058a4882c64fe384aaa1d7fdc9
VIPABH <a891983.2@gmail.com>
2025-11-21 15:13:28 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ from ABH import *
 from telethon import events
 @ABH.on(events.NewMessage)
 async def handle_commands(e):
+    print(e)
     media = e.media
     if media:
         return await e.reply('x')
-        
