8b2700b96de39a56053f8d571ffd9bab56cad5ac
VIPABH <a891983.2@gmail.com>
2025-11-25 23:29:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -55,4 +55,3 @@ async def yt_func(e):
             os.remove(thumb)
     except Exception as e:
         print(f"حدث خطأ أثناء تحميل الفيديو: {e}")
-        await e.reply("حدث خطأ أثناء التحميل، يرجى المحاولة لاحقًا.")
