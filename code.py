0949834c147aa2fea8ea54ae0649ce79a0a2969c
VIPABH <a891983.2@gmail.com>
2025-11-25 23:17:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,58 @@
-from ABH import *
-from telethon import events
-
-# تخزين آخر media group تمت معالجته
-processed_groups = set()
-
-@ABH.on(events.NewMessage)
-async def handle_commands(e):
-    # إذا توجد وسائط
-    if e.media:
-        # إذا الرسالة جزء من media group
-        gid = getattr(e, 'grouped_id', None)
-
-        if gid:
-            # إذا سبق الرد على هذا الـ group → لا تكرر الرد
-            if gid in processed_groups:
-                return
-            processed_groups.add(gid)
-
-        # ردّ واحد فقط
-        return await e.reply('x')
+import yt_dlp, os, re, time, wget, json
+from youtube_search import YoutubeSearch as Y88F8
+from ABH  import *
+@ABH.on(events.NewMessage(pattern=r'^(يوت|yt|حمل|تحميل)\s*(.*)$'))
+async def yt_func(e):
+    query = e.pattern_match.group(2)
+    if not query:
+        r = await e.get_reply_message()
+        if r and r.text:
+            query = r.text
+        else:
+            return await e.reply("😑")
+    results = Y88F8(query, max_results=1).to_dict()
+    if not results:
+        return await e.reply("لم يتم العثور على نتائج.")
+    res = results[0]
+        # if ytdb.get(f'ytvideo{res["id"]}'):
+        #     aud = ytdb.get(f'ytvideo{res["id"]}')
+        #     duration_string = time.strftime('%M:%S', time.gmtime(aud["duration"]))
+        #     return e.reply_audio(
+        #         aud["audio"],
+        #         caption=f'@{channel} ~ {duration_string} ⏳',
+        #         reply_markup=rep
+        #     )
+    url = f'https://youtu.be/{res["id"]}'
+    ydl_ops = {
+        "format": "bestaudio[ext=m4a]",
+        "username": os.environ.get("u"),
+        "password": os.environ.get("p"),
+        "forceduration": True,
+        "noplaylist": True
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info = ydl.extract_info(url, download=False)
+            title = info.get('title')
+            duration = info.get('duration')
+            thumbnail = info.get('thumbnail')
+            uploader = info.get('uploader')
+            duration_string = time.strftime('%M:%S', time.gmtime(duration))
+            audio_file = ydl.prepare_filename(info)
+            ydl.download([url])
+            os.rename(audio_file, audio_file.replace(".m4a", ".mp3"))
+            audio_file = audio_file.replace(".m4a", ".mp3")
+            thumb = wget.download(thumbnail)
+            a = e.reply_audio(
+                audio_file,
+            )
+            # ytdb.set(f'ytvideo{res["id"]}', {
+            #     "type": "audio",
+            #     "audio": a.audio.file_id,
+            #     "duration": a.audio.duration
+            # })
+            os.remove(audio_file)
+            os.remove(thumb)
+    except Exception as e:
+        print(f"حدث خطأ أثناء تحميل الفيديو: {e}")
+        await e.reply("حدث خطأ أثناء التحميل، يرجى المحاولة لاحقًا.")
