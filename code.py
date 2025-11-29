57c3105ea8e7b84c683906e251f4a3a39fae7fbf
VIPABH <a891983.2@gmail.com>
2025-11-30 01:22:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,14 @@
-from telethon import events
-from Resources import *
-from ABH import ABH
-@ABH.on(events.MessageEdited)
-async def x(e):
-    await hint(str(e))
+@client.on(events.MessageEdited)
+async def edited_msg(event):
+    msg_id = event.message.id
+
+    old_text = messages_cache.get(msg_id, None)      # النص قبل التعديل
+    new_text = event.message.message                 # النص بعد التعديل
+
+    # حدث التحديث في التخزين
+    messages_cache[msg_id] = new_text
+
+    await event.reply(
+        f"النص قبل التعديل:\n{old_text}\n\n"
+        f"النص بعد التعديل:\n{new_text}"
+    )
