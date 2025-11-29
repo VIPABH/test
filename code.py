5d713173dd0500fd10c683a2b99548ca03b63972
VIPABH <a891983.2@gmail.com>
2025-11-29 12:40:03 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
 from telethon import TelegramClient, events
-import os
-ttl_seconds = 10
-api_id = int(os.environ.get('API_ID'))
-api_hash = os.environ.get('API_HASH')
+
+ttl_seconds = 10  # مدة التدمير الذاتي
 
 user = TelegramClient('user', api_id, api_hash)
 
-@user.on(events.NewMessage(outgoing=True))
+@user.on(events.NewMessage)
 async def s(e):
     await user.send_file(
         e.chat_id,
         file='موارد/photo_2025-02-10_11-40-17.jpg',
-        supports_streaming=True,
-        ttl_seconds=ttl_seconds
+        supports_streaming=False,      # لأننا نرسل صورة وليس فيديو
+        protected_content=True,        # حماية الصورة من التحميل وإعادة التوجيه
+        ttl_seconds=ttl_seconds        # تفعيل التدمير الذاتي
     )
 
 user.start()
