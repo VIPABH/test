dbf89c0a2a7e402673c6c7255970902a0e6706f0
VIPABH <a891983.2@gmail.com>
2025-11-30 00:56:32 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,10 +5,9 @@ from ABH import ABH
 async def handler(e):
     text = e.text
     x = create('audio_data.json')
-    for title, info in x.items():
-        if title.startswith(text):
-            await ABH.forward_messages(e.chat_id, info["message_id"], info["chat_id"])
-            return
+    if text in x:
+        await ABH.forward_messages(e.chat_id, info["message_id"], info["chat_id"])
+        return
     if text == "لطميات":
         data = ""
         for t, info in x.items():
