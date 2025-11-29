7dffc038f0c52bfbf74c18a9a2ba6a66f2ca0c01
VIPABH <a891983.2@gmail.com>
2025-11-30 00:51:02 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,62 +1,15 @@
-from ABH import ABH
 from telethon import events
-import asyncio
-import json
-import os
-
-JSON_FILE = "audio_data.json"
-x = {}  # title -> {"chat_id": ..., "message_id": ...}
-
-if os.path.exists(JSON_FILE):
-    with open(JSON_FILE, "r", encoding="utf-8") as f:
-        x = json.load(f)
-
-async def save_json():
-    with open(JSON_FILE, "w", encoding="utf-8") as f:
-        json.dump(x, f, ensure_ascii=False, indent=4)
-
-async def send_in_chunks(chat_id, text):
-    chunk_size = 4096
-    for i in range(0, len(text), chunk_size):
-        await ABH.send_message(chat_id, text[i:i+chunk_size])
-
+from Resources import *
+from ABH import ABH
 @ABH.on(events.NewMessage)
 async def handler(e):
-    
-    # البحث أول مرة فقط إذا JSON فارغ
-    if not x:
-        for i in range(50, 502):
-            msg = await ABH.get_messages("x04ou", ids=i)
-            await asyncio.sleep(0.05)
-
-            if not msg or not msg.file:
-                continue
-
-            title = msg.file.title
-            if not title:
-                continue
-
-            x[title.lower()] = {
-                "chat_id": "x04ou",
-                "message_id": msg.id
-            }
-
-        await save_json()
-
-    text = e.text.lower()
-    if text == ".":
-        await ABH.send_file(e.chat_id, "موارد/photo_2025-02-10_11-40-17.jpg", ttl=10)
-    if not len(text) > 2: 
-        return
-    # تطابق يبدأ من البداية
+    text = e.text
+    x = create('audio_data.json')
     for title, info in x.items():
         if title.startswith(text):
-            # إرسال الملف عن طريق forward_messages
             await ABH.forward_messages(e.chat_id, info["message_id"], info["chat_id"])
             return
-
-    if text == "دز":
+    if text == "لطميات":
         data = ""
         for t, info in x.items():
             data += f"{t} → chat: {info['chat_id']}, msg_id: {info['message_id']}\n"
-        await send_in_chunks(e.chat_id, data)
