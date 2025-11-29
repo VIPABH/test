b53976f20398ca84ba64b8b285658a9721bd80a5
VIPABH <a891983.2@gmail.com>
2025-11-29 12:34:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,17 @@
-import yt_dlp, os, re, time, json, requests
-from youtube_search import YoutubeSearch as Y88F8
-from ABH import *
-from Resources import *
+from telethon import TelegramClient, events
 
-ttl_seconds = 10  # مدة التدمير الذاتي بالثواني (غيرها مثل ما تريد)
+ttl_seconds = 10
+
+user = TelegramClient('user', api_id, api_hash)
 
 @user.on(events.NewMessage(outgoing=True))
 async def s(e):
-    await ABH.send_file(
+    await user.send_file(
         e.chat_id,
         file='موارد/photo_2025-02-10_11-40-17.jpg',
-        # video_note=False,
-        # supports_streaming=True,
+        supports_streaming=True,
         ttl_seconds=ttl_seconds
     )
+
+user.start()
+user.run_until_disconnected()
