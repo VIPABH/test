af7525d3fc6cfb15129c149f95b60bdd5ebc41db
VIPABH <a891983.2@gmail.com>
2025-11-30 01:27:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,6 @@ from telethon import events
 from Resources import *
 from ABH import ABH
 messages_cache = {}
-@client.on(events.MessageDeleted)
+@ABH.on(events.MessageDeleted)
 async def handler(event):
     print(event.deleted_ids)
