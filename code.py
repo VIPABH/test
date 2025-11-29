87f57e87437baee26ed7f025254610fa6705b7da
VIPABH <a891983.2@gmail.com>
2025-11-29 18:12:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,17 @@
-# from telethon import TelegramClient, events
-# import os
-# ttl_seconds = 10  # مدة التدمير الذاتي
-# api_id = int(os.environ.get('API_ID'))
-# api_hash = os.environ.get('API_HASH')
-
-# user = TelegramClient('user', api_id, api_hash)
-
-# @user.on(events.NewMessage)
-# async def s(e):
-#     await user.send_file(
-#         e.chat_id,
-#         file='موارد/photo_2025-02-10_11-40-17.jpg',
-#         supports_streaming=False,      # لأننا نرسل صورة وليس فيديو
-#         protected_content=True,        # حماية الصورة من التحميل وإعادة التوجيه
-#         ttl_seconds=ttl_seconds        # تفعيل التدمير الذاتي
-#     )
-
-# user.start()
-# user.run_until_disconnected()
+from ABH import ABH
+x = {}
+@ABH.on(events.NewMessage)
+async def x(e):
+  for i in range(50, 502):
+    msg = await ABH.get_messages("x04ou", ids=i)
+    if not msg or not msg.file:
+        continue
+    title = msg.file.title
+    if tiltle not in x:
+      url = msg.file.url
+      x[title] = link
+    text = e.text
+    if text in x:
+      await ABH.send_file(e.chat_id, x[title])
+    elif text == "دز":
+      await e.reply(str(x))
