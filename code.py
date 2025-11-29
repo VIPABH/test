b65251a32b1a23d0381fb88e43e4ed4404778f39
VIPABH <a891983.2@gmail.com>
2025-11-29 12:40:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
-from telethon import TelegramClient, events
-import os
-ttl_seconds = 10  # مدة التدمير الذاتي
-api_id = int(os.environ.get('API_ID'))
-api_hash = os.environ.get('API_HASH')
+# from telethon import TelegramClient, events
+# import os
+# ttl_seconds = 10  # مدة التدمير الذاتي
+# api_id = int(os.environ.get('API_ID'))
+# api_hash = os.environ.get('API_HASH')
 
-user = TelegramClient('user', api_id, api_hash)
+# user = TelegramClient('user', api_id, api_hash)
 
-@user.on(events.NewMessage)
-async def s(e):
-    await user.send_file(
-        e.chat_id,
-        file='موارد/photo_2025-02-10_11-40-17.jpg',
-        supports_streaming=False,      # لأننا نرسل صورة وليس فيديو
-        protected_content=True,        # حماية الصورة من التحميل وإعادة التوجيه
-        ttl_seconds=ttl_seconds        # تفعيل التدمير الذاتي
-    )
+# @user.on(events.NewMessage)
+# async def s(e):
+#     await user.send_file(
+#         e.chat_id,
+#         file='موارد/photo_2025-02-10_11-40-17.jpg',
+#         supports_streaming=False,      # لأننا نرسل صورة وليس فيديو
+#         protected_content=True,        # حماية الصورة من التحميل وإعادة التوجيه
+#         ttl_seconds=ttl_seconds        # تفعيل التدمير الذاتي
+#     )
 
-user.start()
-user.run_until_disconnected()
+# user.start()
+# user.run_until_disconnected()
