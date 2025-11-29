82c4acd57fe291e329e2803a44bf6a9ee4825517
VIPABH <a891983.2@gmail.com>
2025-11-29 19:14:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ async def handler(e):
     if not x:
         for i in range(50, 502):
             msg = await ABH.get_messages("x04ou", ids=i)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.05)
             if not msg or not msg.file:
                 continue
 
