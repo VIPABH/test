a70c5ebc630eb482e937a860309adfb941a3b75d
VIPABH <a891983.2@gmail.com>
2025-11-30 21:50:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,21 @@ from Resources import *
 from ABH import ABH
 messages_cache = {}
 @ABH.on(events.NewMessage)
-async def handler(event):
-    print(event)
-    await ABH.send_file(event.chat_id, event.media.document)
+async def مستمع_اللطميات(e):
+    text = e.text.strip()
+    if text not in لطميات:
+        return
+    msg_id = لطميات[text]["message_id"]
+    b = Button.url('❤', url=f'https://t.me/x04ou/{msg_id}')
+    msgs = await ABH.get_messages('x04ou', ids=[msg_id])
+    if not msgs:
+        return
+    msg = msgs[0]
+    await ABH.send_file(e.chat_id, msg, reply_to=e.id, buttons=b)
+button = [Button.inline('التالي', data=f'next'), Button.inline('السابق', data=f'retrunback')]
+@ABH.on(events.NewMessage(pattern='^لطميات$', from_users=[wfffp]))
+async def listlatmeat(e):
+    msg = ''
+    for name, data in list(لطميات.items())[:50]:
+        msg += f'( `{name}` )\n'
+    await chs(e, msg)
