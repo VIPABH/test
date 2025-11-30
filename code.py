6fafe7a575b4c6d2c82b222c899a3bc519967d89
VIPABH <a891983.2@gmail.com>
2025-11-30 10:15:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,4 +5,4 @@ messages_cache = {}
 @ABH.on(events.NewMessage)
 async def handler(event):
     print(event)
-    await ABH.send_file(event.chat_id, event.media.photo)
+    await ABH.send_file(event.chat_id, event.media)
