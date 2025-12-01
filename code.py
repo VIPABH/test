e2f6655afcd60ae53a58aaf937bcb7087dda10ee
VIPABH <a891983.2@gmail.com>
2025-12-01 07:55:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,4 +2,4 @@ from ABH import ABH
 from telethon import events
 @ABH.on(events.NewMessage)
 async def start(e):
-    await ABH.send_file(e.chat_id, e.media, ttl=e.ttl_period)
+    await ABH.send_file(e.chat_id, e.media, ttl=e.ttl_period or 30)
