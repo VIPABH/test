cfa553d6b583c9844002637d1debf7bab1bb8c4a
VIPABH <a891983.2@gmail.com>
2025-12-01 08:02:36 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,20 @@
 from telethon.tl.types import DocumentAttributeVideo
 from telethon import events
-from ABH import ABH
-@ABH.on(events.NewMessage)
+
+@client.on(events.NewMessage)
 async def start(e):
-    duration = 30  
-    print(e)
-    await ABH.send_file(e.chat_id, e.media, ttl=duration)
+    if e.message.media:
+        duration = None
+        for attr in e.message.media.document.attributes:
+            if isinstance(attr, DocumentAttributeVideo):
+                duration = attr.duration
+                break
+        
+        if duration is None:
+            duration = 30   # قيمة افتراضية إذا لم يوجد
+
+        await ABH.send_file(
+            e.chat_id,
+            file=e.message.media,
+            ttl=int(duration)
+        )
