1f671d0b133f0490370493f308ffca3ed88446ee
VIPABH <a891983.2@gmail.com>
2025-12-01 08:05:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,17 +5,18 @@ from ABH import ABH
 @ABH.on(events.NewMessage)
 async def start(e):
     if e.message.media:
-        duration = None
-        for attr in e.message.media.document.attributes:
-            if isinstance(attr, DocumentAttributeVideo):
-                duration = attr.duration
-                break
-        
-        if duration is None:
-            duration = 30   # قيمة افتراضية إذا لم يوجد
+
+        ttl = 30   # القيمة الافتراضية للصور والوسائط العادية
+
+        # التحقق إذا كانت الوسائط Video
+        if hasattr(e.message.media, "document") and e.message.media.document:
+            for attr in e.message.media.document.attributes:
+                if isinstance(attr, DocumentAttributeVideo):
+                    ttl = attr.duration or 30
+                    break
 
         await ABH.send_file(
             e.chat_id,
             file=e.message.media,
-            ttl=int(duration)
+            ttl=int(ttl)
         )
