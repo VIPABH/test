c5ae2c3a0a4ca9361b6d5be970709a5be3421eee
VIPABH <a891983.2@gmail.com>
2025-12-02 23:48:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,55 @@
-from telethon.tl.types import DocumentAttributeVideo
-from telethon import events
-from ABH import ABH
-
-@ABH.on(events.NewMessage)
-async def start(e):
-    if e.message.media:
-
-        ttl = 30   # القيمة الافتراضية للصور والوسائط العادية
-
-        # التحقق إذا كانت الوسائط Video
-        if hasattr(e.message.media, "document") and e.message.media.document:
-            for attr in e.message.media.document.attributes:
-                if isinstance(attr, DocumentAttributeVideo):
-                    ttl = attr.duration or 30
-                    break
-
-        await ABH.send_file(
-            e.chat_id,
-            file=e.message.media,
-            ttl=int(ttl)
-        )
+import yt_dlp, os, time, wget
+from youtube_search import YoutubeSearch as Y88F8
+from threading import Thread
+from ABH import *
+@ABH.on(events.NewMessage(pattern=r'^(حمل|يوت|تحميل|yt) (.+)'))
+def ytdownloaderHandler(e):
+    Thread(target=yt_func, args=(e)).start()
+async def yt_func(e):
+    re = await e.get_reply_message()
+    query = e.pattern_match.group(2)
+    if not query:
+        if re:
+            query = re.text
+        else:
+            await e.reply('شنو تحب احملك وانت ما كاتب بحث')
+    results = Y88F8(query, max_results=1).to_dict()
+    if not results:
+        return e.reply("لم يتم العثور على نتائج.")
+    res = results[0]
+    if r.get(f'ytvideo{res["id"]}'):
+        aud = r.get(f'ytvideo{res["id"]}')
+        duration_string = time.strftime('%M:%S', time.gmtime(aud["duration"]))
+    url = f'https://youtu.be/{res["id"]}'
+    ydl_ops = {
+        "format": "bestaudio[ext=m4a]",
+        "username": os.environ.get("u"),
+        "password": os.environ.get("p"),
+        "forceduration": True,
+        "noplaylist": True
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info = ydl.extract_info(url, download=False)
+            duration = info.get('duration')
+            thumbnail = info.get('thumbnail')
+            audio_file = ydl.prepare_filename(info)
+            ydl.download([url])
+            os.rename(audio_file, audio_file.replace(".m4a", ".mp3"))
+            audio_file = audio_file.replace(".m4a", ".mp3")
+            thumb = wget.download(thumbnail)
+            a = await ABH.send_file(
+                e.chat_id,
+                audio_file,
+                caption="[ENJOY DEAR](https://t.me/VIPABH_BOT)"
+                )
+            r.set(f'ytvideo{res["id"]}', {
+                "type": "audio",
+                "audio": a.audio.file_id,
+                "duration": a.audio.duration
+            })
+            os.remove(audio_file)
+            os.remove(thumb)
+    except Exception as e:
+        print(f"حدث خطأ أثناء تحميل الفيديو: {e}")
+        m.reply("حدث خطأ أثناء التحميل، يرجى المحاولة لاحقًا.")
