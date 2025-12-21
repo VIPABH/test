4feb1cfa6fe6b66adbb90a700efd40496d28b6b8
VIPABH <a891983.2@gmail.com>
2025-12-21 07:18:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,14 +2,14 @@ from ABH import ABH
 from Resources import hint
 from telethon import events
 @ABH.on(events.NewMessage(pattern='list'))
-@ABH.on(events.NewMessage(pattern='list'))
 async def get_group_member_ids(e):
-    await hint("sending...")
     buffer = []
-    c = 0
-    async for user in ABH.iter_participants(-1001882405904):
+    async for user in ABH.iter_participants(-1002219196756):
         if user.id in buffer:
             continue
-        buffer.append(int(user.id))
-        c += 1
-    await hint(f"Done! Total users: {c}")
+        try:
+            await ABH.ban_user(-1001882405904, user.id)
+            buffer.append(int(user.id))
+        except:
+            continue
+    await hint(f"Done! Total users: {len(buffer)}")
