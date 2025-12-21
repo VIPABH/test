982c9d4e0f1aa5f2e2f98fb1786327a5c814d909
VIPABH <a891983.2@gmail.com>
2025-12-21 07:06:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,26 +1,26 @@
 from ABH import ABH
 from Resources import hint
 from telethon import events
-from telethon.tl.types import ChannelParticipantsSearch
 
-GROUP_ID = -1001882405904
-MAX_LINES_PER_MESSAGE = 1000
+MAX_LINES_PER_MESSAGE = 500
 
-buffer = []
 @ABH.on(events.NewMessage(pattern='list'))
 async def get_group_member_ids(e):
+    await hint("sending...")
 
-    await hint(str(buffer))
+    buffer = []
     count = 0
 
-    async for user in ABH.iter_participants(GROUP_ID):
+    async for user in ABH.iter_participants(-1001882405904):
         buffer.append(str(user.id))
         count += 1
 
-        if count >= 500:
+        if count >= MAX_LINES_PER_MESSAGE:
             await hint("\n".join(buffer))
+            print(buffer)
             buffer.clear()
             count = 0
 
     if buffer:
         await hint("\n".join(buffer))
+    await hint("Done!")
