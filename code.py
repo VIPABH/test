e8c5aec1ef0aef7ae96725d3b810294bcb000f66
VIPABH <a891983.2@gmail.com>
2026-01-13 10:14:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,9 @@ from Resources import wfffp
 from ABH import ABH
 
 def is_allowed_sender(event):
-    return event.sender_id == wfffp
+    return event.sender_id
+
+
 
 @ABH.on(events.NewMessage(func=is_allowed_sender))
 async def auto_reply(event):
