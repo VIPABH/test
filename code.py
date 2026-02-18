abd5517f0a25c7a1f1c7b024dd5a2a19b1ecdfa8
VIPABH <a891983.2@gmail.com>
2026-02-18 19:05:27 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,9 @@
 from ABH import *
-from art import text2art
-import random
-
-# دالة لتجهيز النص الفني لتليجرام
-def get_telegram_banner(text):
-    # تحويل النص إلى فن ASCII
-    ascii_text = text2art(text, font="big") # خط small أفضل للهواتف
-    # وضع النص داخل علامات الكود لضمان ترتيب المسافات
-    return ascii_text
-
-@ABH.on(events.NewMessage(pattern=r'^البداية$'))
-async def start_msg(e):
-    banner = get_telegram_banner("ABH")
-    
-    msg = f"{banner}\n🚀 **أهلاً بك في البوت!**\n\n💡"
-    await e.reply(msg)
-    
+b = [
+    Button.inline("اضف قناة", data="add"),
+    Button.inline("حذف قناة", data="del"),
+    Button.inline("عرض القنوات", data="get"),
+@ABH.on(events.NewMessage(pattern="^/start"))
+async def start(e):
+    if not e.is_private:
+        return
