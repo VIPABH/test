de81bddb4636152946b8a43955171eaeac0b3f50
VIPABH <a891983.2@gmail.com>
2026-03-11 13:21:10 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,49 +1,18 @@
-import asyncio
-import os
-from telethon import events
-from faster_whisper import WhisperModel 
 from ABH import *
-model = WhisperModel(
-    "distil-large-v3", 
-    device="cpu", 
-    compute_type="float32", 
-    cpu_threads=4, 
-    num_workers=2
-)
-@ABH.on(events.NewMessage)
-async def handle_audio(event):
-    print("on")
-    if event.voice or event.audio:
-        if event.file.size > 10 * 1024 * 1024: # 10 ميجا بايت
-            return await event.reply("الملف كبير جداً، يرجى إرسال مقطع أقل من 10 دقائق.")
-
-        msg = await event.reply("🚀 جاري المعالجة بأقصى دقة...")
-        path = await event.download_media()
-        
-        try:
-            # beam_size=1 مع distil-models يعطي سرعة خارقة ودقة ممتازة
-            # task="transcribe" لضمان الكتابة بنفس لغة الصوت
-            segments, info = await asyncio.to_thread(
-                model.transcribe, 
-                path, 
-                beam_size=1, 
-                language=None, # التعرف التلقائي على اللغة
-                vad_filter=True # تصفية الصمت والضوضاء لزيادة السرعة
-            )
-            full_text = "".join([segment.text for segment in segments])
-
-            if not full_text.strip():
-                await msg.edit("❌ لم أتمكن من فهم الكلام.")
-            else:
-                response = (f"**✅ تمت المعالجة بنجاح**\n"
-                            f"**اللغة:** {info.language}\n"
-                            f"**الدقة:** {info.language_probability:.2%}\n\n"
-                            f"**النص:**\n`{full_text.strip()}`")
-                await msg.edit(response)
-        
-        except Exception as e:
-            await msg.edit(f"⚠️ خطأ: {str(e)}")
-        
-        finally:
-            if os.path.exists(path):
-                os.remove(path)
+from shortcut import *
+from telethon import Button
+async def check_force_sub(user_id, channel_username):
+    try:
+        participant = await ABH.get_participants(channel_username, filter=lambda p: p.id == user_id)
+        return True
+    except Exception:
+        return False
+@ABH.on(events.NewMessage(pattern="^/start$"))
+async def start(e):
+    if not e.is_private:
+        return
+    if not check_force_sub(e.sender_id, "x04ou"):
+        b = Button.url('القناة', url='https://t.me/x04ou')
+        return await e.reply("عذرا بس انت ما مشترك بالقناة")
+    else:
+        return await e.reply('تم التحقق من الاشتراك')
