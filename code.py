148d33771d314aeab5c5bdbbbbb4493faf8e4e45
VIPABH <a891983.2@gmail.com>
2026-03-13 14:01:21 +0300

    Update code.py

--- code.py
+++ code.py
@@ -20,8 +20,9 @@ async def _(e):
         message=""
     ))
 
-    buttons = [
-        [Button.user("بروفايل", user_id=e.sender_id)]
+    await e.reply(
+    "اضغط لفتح البروفايل",
+    buttons=[
+        [Button.url("فتح", f"tg://user?id={e.sender_id}")]
     ]
-    
-    await e.reply("اضغط للدخول للبروفايل:", buttons=buttons)
+)
