c1fd405f89f603056af6f12d8f1c682be02279ea
VIPABH <a891983.2@gmail.com>
2026-03-13 13:53:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,19 @@
 from telethon import events, functions, types
 from ABH import *
+from telethon import events, functions, types
 
 @ABH.on(events.NewMessage(pattern="^/start$"))
 async def _(e):
 
-    invoice = types.Invoice(
-        currency="XTR",
-        prices=[types.LabeledPrice(label="price", amount=50)]
-    )
-
-    await ABH(functions.messages.SendMediaRequest(
+    await ABH(functions.messages.SendInvoiceRequest(
         peer=e.sender_id,
-        media=types.InputMediaInvoice(
-            title="شراء خدمة",
-            description="شراء ميزة في البوت",
-            invoice=invoice,
-            payload=b"buy_service",
-            provider_data=b""
-        ),
-        message=""
+        title="شراء خدمة",
+        description="شراء ميزة في البوت",
+        currency="XTR",
+        prices=[
+            types.LabeledPrice(label="price", amount=50)
+        ],
+        payload=b"buy_service",
+        provider_token="",   # فارغ عند استخدام XTR
+        start_param="start"
     ))
