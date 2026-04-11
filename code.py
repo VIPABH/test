afc39f3acf6b99f05444f843ddf66c9983065e8d
VIPABH <a891983.2@gmail.com>
2026-04-11 09:45:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,18 +2,17 @@ from telethon import events, types
 from ABH import *
 @ABH.on(events.NewMessage)
 async def handler(event):
-    if event.video:
-        video = event.media.document
-        file_id = video.id
-        access_hash = video.access_hash
-        file_reference = video.file_reference
-        input_document = types.InputDocument(
-            id=file_id,
-            access_hash=access_hash,
-            file_reference=file_reference
-        )
-        await ABH.send_file(
-            event.chat_id,
-            input_document,
-            caption="تم الإرسال عبر الـ Access Hash"
-        )
+    video = event.media.document
+    file_id = video.id
+    access_hash = video.access_hash
+    file_reference = video.file_reference
+    input_document = types.InputDocument(
+        id=file_id,
+        access_hash=access_hash,
+        file_reference=file_reference
+    )
+    await ABH.send_file(
+        event.chat_id,
+        input_document,
+        caption="تم الإرسال عبر الـ Access Hash"
+    )
