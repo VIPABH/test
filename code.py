97d295607f53686638659848356283c8fe2f18c7
VIPABH <a891983.2@gmail.com>
2026-04-18 10:48:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,24 @@
 from ABH import *
 x = []
+
 @ABH.on(events.NewMessage)
 async def handler(e):
     id = e.sender_id
     if id not in x:
         x.append(id)
         await e.reply("Hello")
+    
     if len(x) == 10:
-        y = await ABH.get_entity(x)
-        await e.reply(y)
+        # الحصول على كائنات المستخدمين للقائمة كاملة
+        users = await ABH.get_entity(x)
+        
+        # تحويل القائمة إلى نص يحتوي على أسماء المستخدمين مثلاً
+        msg_text = "قائمة أول 10 مستخدمين تواصلوا معي:\n"
+        for user in users:
+            msg_text += f"- {user.first_name} (ID: {user.id})\n"
+        
+        # الآن نرسل النص (String) وليس الكائن
+        await e.reply(msg_text)
+        
+        # اختيار اختياري: تصفير القائمة إذا أردت البدء من جديد
+        # x.clear()
