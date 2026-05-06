44dd3b560d0e38c4aa2181d01271f3fae06b14a4
VIPABH <a891983.2@gmail.com>
2026-05-06 15:18:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,4 +4,4 @@ session = {}
 @ABH.on(events.NewMessage(pattern=r'^اهمس (.*)'))
 async def whisper_list_handler(e):
     input_data = e.pattern_match.group(1).split()    
-    await e.reply(input_data)
+    await e.reply(type(input_data))
