ef41c46be82cc35b8c6495422dd744ee66f4b920
VIPABH <a891983.2@gmail.com>
2026-05-06 15:15:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,12 @@
 from Resources import *
 from ABH import *
+
 session = {}
+
 @ABH.on(events.NewMessage(pattern=r'^اهمس (.*)'))
 async def whisper_list_handler(e):
-    input_data = e.pattern_match.group(1).strip()
-    await e.reply(input_data)
+    # استخدام .split() لتحويل النص إلى قائمة كلمات بناءً على المسافات
+    input_data = e.pattern_match.group(1).strip().split()
+    
+    # الآن input_data هي قائمة (List) بكل الكلمات بعد كلمة 'اهمس'
+    await e.reply(str(input_data))
