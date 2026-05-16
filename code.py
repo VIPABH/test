d7fa04b79a7daa50051a91dde2383bbdc3c4f07a
VIPABH <a891983.2@gmail.com>
2026-05-16 10:30:12 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,76 +1,65 @@
-# from ABH import *
-# session_aliases = {}
-# @ABH.on(events.NewMessage(incoming=True))
-# async def execute_alias_engine(event):
-#     chat_id = event.chat_id
-#     text = event.raw_text
-#     parts = text.split(maxsplit=1)
-#     if not parts:
-#         return
-#     incoming_shortcut = parts[0].lower()
-#     args = parts[1] if len(parts) > 1 else ""    
-#     real_cmd = r.hget(f"cmd:{chat_id}", incoming_shortcut)    
-#     if real_cmd:
-#         event.raw_text = f"{real_cmd} {args}"        
-#         try:
-#             event._parse_msg()
-#             await ABH._dispatch_event(event)
-#         except:
-#             pass
-# @ABH.on(events.NewMessage(pattern=r'^اختصار$'))
-# async def start_alias_session(event):
-#     chat_id, user_id = event.chat_id, event.sender_id
-#     if chat_id not in session_aliases: session_aliases[chat_id] = {}
-#     session_aliases[chat_id][user_id] = {"step": "waiting_old", "old_command": None}
-#     await event.reply('**الآن أرسل الأمر القديم (الأصلي) كما هو تماماً:**')
-#     ABH.add_event_handler(
-#         get_aliases_handler, 
-#         events.NewMessage(chats=chat_id, from_users=user_id)
-#     )    
-#     await asyncio.sleep(60)
-#     if chat_id in session_aliases and user_id in session_aliases[chat_id]:
-#         del session_aliases[chat_id][user_id]
-#         ABH.remove_event_handler(get_aliases_handler)
-# async def get_aliases_handler(event):
-#     chat_id, user_id = event.chat_id, event.sender_id
-#     if chat_id not in session_aliases or user_id not in session_aliases[chat_id]:
-#         return
-#     if event.text =='اختصار': return
-#     session = session_aliases[chat_id][user_id]
-#     if session["step"] == "waiting_old":
-#         session["old_command"] = event.text.strip()
-#         session["step"] = "waiting_new"
-#         await event.reply(f"**حسناً، الأصل هو:** `{session['old_command']}`\n**الآن أرسل كلمة الاختصار الجديدة:**")
-#     elif session["step"] == "waiting_new":
-#         new_alias = event.text.strip().lower()
-#         old_command = session["old_command"]                
-#         r.hset(f"cmd:{chat_id}", new_alias, old_command)
-#         await event.reply(f"✅ تم بنجاح:\n**{new_alias}** ↫ **{old_command}**")        
-#         del session_aliases[chat_id][user_id]
-#         ABH.remove_event_handler(get_aliases_handler)
-# @ABH.on(events.NewMessage(pattern=r'^(حذف|مسح) اختصار (.+)'))
-# async def remove_alias(event):
-#     chat_id = event.chat_id
-#     target = event.pattern_match.group(2).strip().lower()
-#     if r.hdel(f"cmd:{chat_id}", target):
-#         await event.reply(f"🗑 تم حذف الاختصار `{target}`")
-#     else:
-#         await event.reply(f"⚠️ الاختصار غير موجود.")
-# @ABH.on(events.NewMessage(pattern=r'^الاختصارات$'))
-# async def list_aliases(event):
-#     all_as = r.hgetall(f"cmd:{event.chat_id}")
-#     if not all_as: 
-#         return await event.reply("⚠️ لا توجد اختصارات حالياً.")
-#     res = "🔗 **اختصارات الكروب (نص خام):**\n\n"
-#     for s, o in all_as.items():
-#         res += f"• `{o}` ↫ `{s}`\n"
-#     await event.reply(res)
-# @ABH.on(events.NewMessage(pattern=r'^حذف الاختصارات$'))
-# async def clear_all_aliases(event):
-#     r.delete(f"cmd:{event.chat_id}")
-#     await event.reply("✅ تم تصفير جميع الاختصارات.")
-# @ABH.on(events.NewMessage(pattern=r'^(?:\.|/)?(تقييد عام|تقييد|تحذير|طرد|حظر)(?:\s+(.*))?$'))
-# async def execute_alias_engine(event):
-#     الامر = event.pattern_match.group(1).strip().lower()
-#     المعطى = event.pattern_match.group(2) or ""
-#     await event.reply(f'شغال \n {الامر=}, {المعطى}')
+from ABH import *
+from telethon import Button
+@ABH.on(events.NewMessage(pattern="^/start$"))
+async def start(e):
+    if e.is_private:
+        id = e.sender_id
+        if id == 1910015590:
+            admin_buttons = [
+                [
+                    Button.inline("اوامر النشر", data='posting_commands'),
+                    Button.inline("اوامر المحظورين", data='banned_commands')
+                ],
+                [
+                    Button.inline("اوامر الاشتراك الاجباري", data='subscribe_commands')
+                ]
+            ]
+            return await e.reply(
+                '**أهلاً زعيم، شنو تحب تسوي؟ 👇🏾**', 
+                buttons=admin_buttons
+            )            
+        user_buttons = [
+            Button.url("➕ أضفني لمجموعتك", url=f"https://t.me/vipabh_bot?startgroup=true&admin=ban_users+delete_messages+restrict_members+invite_users+pin_messages+change_info"),
+            Button.url("📢 القناة", url=f"https://t.me/{CHANNEL_KEY}")
+    ]
+        await e.reply(
+            "أهلاً بك! أنا صانع بوتات.", 
+            buttons=user_buttons
+        )
+callbacknames = {
+    'posting_commands': 'اوامر النشر', 
+    'banned_commands': "اوامر المحظورين", 
+    'subscribe_commands': 'اوامر الاشتراك الاجباري'
+}
+@ABH.on(events.CallbackQuery(pattern=r'(posting_commands|banned_commands|subscribe_commands)'))
+async def callback_handler(event):
+    data = event.data.decode()    
+    if data == 'posting_commands':
+        buttons = [
+            [
+                Button.inline("انشاء رسالة", data='creat_message'),
+                Button.inline("التقاط رسالة", data='catch_message')
+            ]
+        ]
+    elif data == 'banned_commands':
+        buttons = [
+            [
+                Button.inline("حظر عضو", data='ban_user'),
+                Button.inline("الغاء حظر عضو", data='unban_user')
+            ],
+            [
+                Button.inline("المحظورين", data='banned_users')
+            ]
+        ]
+    elif data == 'subscribe_commands':
+        userslen = r.scard('users')
+        buttons = [
+            [
+                Button.inline(f"المستخدمين ( {userslen} )", data='ban_user'),
+                Button.inline("جلب اسامي المستخدمين", data='banned_users')
+            ],
+            [
+                Button.inline("البحث عن مستخدم", data='unban_user')
+            ]
+        ]        
+    await event.edit(f'اختر احد الازرار من قائمة {callbacknames[data]}', buttons=buttons)
