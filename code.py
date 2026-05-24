56f74e3df890bd6b6584b0ef90f5be66d513e07a
VIPABH <a891983.2@gmail.com>
2026-05-24 10:23:18 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,29 +1,19 @@
 import os
-from telethon import TelegramClient, events, types # أضفنا types هنا للتأكيد
+from telethon import TelegramClient, events
 from faster_whisper import WhisperModel
 from ABH import *
 from Resources import *
 
 @ABH.on(events.NewMessage(incoming=True))
 async def send_larger_hint(event):
-    text = "الرقم الصحيح أكبر"
-    # نستخدم مسافة عادية كقاعدة للإيموجي لمنع تضارب الـ UTF-16
-    placeholder = " " 
-    
     CUSTOM_EMOJI_ID = 5276514176657812074 
     
-    # حساب الموقع بدقة بالاعتماد على الترميز الصافي
-    text_utf16_len = len(text.encode('utf-16-le')) // 2
-    placeholder_utf16_len = len(placeholder.encode('utf-16-le')) // 2
-    
-    entity = types.MessageEntityCustomEmoji(
-        offset=text_utf16_len,
-        length=placeholder_utf16_len,
-        document_id=CUSTOM_EMOJI_ID
-    )
+    # نستخدم صيغة التنسيق المدمجة لتليجرام (الإيموجي المخصص يعامل كرابط بروتوكول tg://)
+    # النص المكتوب بين الـ [] هو الذي سيظهر فوقه الإيموجي، وضعنا مسافة سحرية مخفية
+    text = f"الرقم الصحيح أكبر [  ](tg://emoji?id={CUSTOM_EMOJI_ID})"
     
     await ABH.send_message(
         event.chat_id, 
-        text + placeholder, 
-        formatting_entities=[entity]
+        text, 
+        parse_mode='md' # تفعيل الماركداون لمعالجة الرابط كإيموجي مخصص
     )
