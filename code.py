aedda5858fc5cb7200b1a6f0f9e87aaa1b3a6711
VIPABH <a891983.2@gmail.com>
2026-07-08 01:05:10 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,33 @@
+from telethon import Button
 from ABH import *
-import asyncio, yt_dlp, json, os, time, urllib.request
-from telethon.tl.types import DocumentAttributeAudio, InputDocument
-from concurrent.futures import ThreadPoolExecutor
-from Resources import *
-from telethon import events
-from telethon.tl.types import MessageEntityMentionName
-@ABH.on(events.NewMessage(pattern=r".?ترحيب"))
-async def send_welcome(event):
-    user_id = 8829795448  # أيدي الشخص
-    name = "بايو Noor."
-    text = f"نورت قروبنا يـ {name} 🥂✨"
-    start = text.find(name)
-    end = start + len(name)
-    entity = MessageEntityMentionName(offset=start, length=len(name), user_id=user_id)
-    await event.reply(text, formatting_entities=[entity])
+math_session = {}
+button = [
+    [
+        Button.inline("7", data="7"),
+        Button.inline("8", data="8"),
+        Button.inline("9", data="9"),
+        Button.inline("/", data="/")
+    ],
+    [
+        Button.inline("4", data="4"),
+        Button.inline("5", data="5"),
+        Button.inline("6", data="6"),
+        Button.inline("*", data="*")
+    ],
+    [
+        Button.inline("1", data="1"),
+        Button.inline("2", data="2"),
+        Button.inline("3", data="3"),
+        Button.inline("-", data="-")
+    ],
+    [
+        Button.inline("0", data="0"),
+        Button.inline(".", data="."),
+        Button.inline("=", data="="),
+        Button.inline("+", data="+")
+    ]
+]
+@ABH.on(events.NewMessage(pattern="الحاسبة"))
+async def math(e):
+    math_session.setdefault(e.sender_id, {})
+    await e.reply("الحاسبة العلمية شغاله, ادخل معادلتك", buttons=button)
