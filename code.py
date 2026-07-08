b06893bb49bb00e2e38cf94f81cb4f4a3937097e
VIPABH <a891983.2@gmail.com>
2026-07-08 11:57:48 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 from telethon import events, Button
 from ABH import ABH 
+
+# قاموس لتخزين حالة كل مستخدم
 math_session = {}
+
 def get_calc_keyboard():
     return [
         [Button.inline("AC", "AC"), Button.inline("( )", "C"), Button.inline("⌫", "DEL"), Button.inline("÷", "/")],
@@ -9,17 +12,23 @@ def get_calc_keyboard():
         [Button.inline("1", "1"), Button.inline("2", "2"), Button.inline("3", "3"), Button.inline("+", "+")],
         [Button.inline("+/-", "NEG"), Button.inline("0", "0"), Button.inline(".", "."), Button.inline("=", "=")]
     ]
+
 @ABH.on(events.NewMessage(pattern="الحاسبة"))
 async def start_math(e):
     math_session[e.sender_id] = {'num': '', 'par': True}
     await e.reply("🧮 **آلة حاسبة ذكية**", buttons=get_calc_keyboard())
+
 @ABH.on(events.CallbackQuery(pattern=rb'^[0-9+\-*/.=ACDELNEG]+$'))
 async def math_callback(e):
     uid = e.sender_id
-    if uid not in math_session: return await e.answer("اكتب 'الحاسبة' مجدداً")    
+    if uid not in math_session: 
+        return await e.answer("تم حذف الجلسة، أرسل 'الحاسبة' مجدداً")
+    
     data = e.pattern_match.group(0).decode('utf-8')
     s = math_session[uid]
     eq = s['num']
+    display_text = ""
+
     if data == "AC":
         eq = ""
     elif data == "DEL":
@@ -30,18 +39,27 @@ async def math_callback(e):
     elif data == "NEG":
         if eq: 
             try: eq = str(eval(eq) * -1)
-            except: pass
+            except: await e.answer("خطأ!")
     elif data.isdigit() or data == '.':
         eq += data
     elif data in ['+', '-', '*', '/']:
+        # منع تكرار الرموز
         if eq and eq[-1] in ['+', '-', '*', '/']: eq = eq[:-1] + data
         else: eq += data
     elif data == '=':
         try:
             res = eval(eq)
-            eq = str(int(res) if isinstance(res, (int, float)) and (float(res)).is_integer() else res)
+            # تنسيق الناتج: إذا كان عدداً صحيحاً لا نعرض الفاصلة
+            res_str = str(int(res) if isinstance(res, (int, float)) and (float(res)).is_integer() else res)
+            display_text = f"🔢 **النتيجة:**\n`{eq} = {res_str}`"
+            s['num'] = res_str # تخزين الناتج للمواصلة عليه
+            await e.edit(text=display_text, buttons=get_calc_keyboard())
+            await e.answer()
+            return # الخروج لأننا قمنا بالتعديل
         except:
             await e.answer("خطأ رياضي!")
+            return
+
     s['num'] = eq    
     display_text = f"🔢 **المعادلة:**\n`{eq if eq else '0'}`"
     await e.edit(text=display_text, buttons=get_calc_keyboard())
