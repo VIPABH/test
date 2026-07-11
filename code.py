3c603c45ba7dde06abbd0985c2d8de604bc541a2
VIPABH <a891983.2@gmail.com>
2026-07-11 10:10:25 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,164 +2,136 @@ from telethon import events, Button
 from Resources import mention
 from ABH import ABH
 import random, asyncio
-
-killamordersession = {}
-
-@ABH.on(events.NewMessage(pattern=r'(?i)(/killamorder|القاتل والمقتول)$'))
+class Game:
+    def __init__(self, owner_id, msg_id):
+        self.owner = owner_id
+        self.msg_id = msg_id
+        self.players = {} 
+    def add_player(self, user_id, name):
+        self.players[user_id] = {"name": name, "points": 2}
+    def player(self, id):
+        return self.players[id]
+    def remove_player(self, user_id):
+        return self.players.pop(user_id, None)
+    def end(self):
+        return self.players.clear()
+    def is_player(self, user_id):
+        return user_id in self.players
+    def killer(self, id=None):
+        if id is None:
+            return self.killer
+        self.killer = id
+    def del_killer(self):
+        self.killer = None
+killamorder = {}
+@ABH.on(events.NewMessage(pattern='(/killamorder|القاتل والمقتول)$'))
 async def killamorderstart(e):
     chat = e.chat_id
     user = e.sender_id
-    
-    if chat in killamordersession:
+    if chat in game:
         return await e.reply("اللعبة قيد التشغيل بالفعل")
-
     m = await mention(e)
-    killamordersession[chat] = {
-        "owner": user,
-        "players": {user: {"name": m, "points": 2}},
-    }
-
-    msg = await e.reply("تم تشغيل لعبة القاتل والمقتول — أرسل (انا) للانضمام")
-    killamordersession[chat]["edit"] = msg.id
-
-
-@ABH.on(events.NewMessage(pattern=r'^انا$'))
-async def register_player(e):
-    chat = e.chat_id
-    user = e.sender_id
-    
-    if chat not in killamordersession:
-        return
-
-    players = killamordersession[chat]["players"]
-
-    if user in players:
-        return await e.reply("سجلتك مسبقًا")
-
+    my = await ABH.get_me()
+    b = Button.url('اضغط هنا للانضمام', url=f"https://t.me/{my.username}?start=killamorder:{chat}")
+    msg = await e.reply("تم تشغيل لعبة القاتل والمقتول ", buttons=[b])
+    game = Game(owner=user, msg_id=msg.id)
+    killamorder[chat] = game
+    game.add_player(user, m)
+@ABH.on(events.NewMessage(pattern='killamorder:([0-9]+)$'))
+async def start_game(e):
+    chat = e.text.replace('killamorder:', '')
+    if chat not in killamorder:
+        return await e.reply("لا توجد لعبة شغالة")
+    game = killamorder[chat]
     m = await mention(e)
-    players[user] = {"name": m, "points": 2}
-
-    # تحديث قائمة اللاعبين
-    msg = "تم تشغيل لعبة القاتل والمقتول — أرسل (انا) للانضمام\n"
-    for P in players.values():
-        msg += f"اللاعب: {P['name']}\n"
-
-    await ABH.edit_message(chat, killamordersession[chat]["edit"], msg)
-    await e.reply(f"تم تسجيلك: {m}")
-
-
+    game.add_player(e.sender_id, m)
+    await e.reply("تم اضافتك للعبة")
 @ABH.on(events.NewMessage(pattern='اللاعبين'))
 async def show_players(e):
     chat = e.chat_id
-
-    if chat not in killamordersession:
+    if chat not in killamorder:
         return await e.reply("لا توجد لعبة شغالة")
-
+    game = killamorder[chat]
     msg = "اللاعبين:\n"
-    for p in killamordersession[chat]["players"].values():
-        msg += f"- {p['name']}\n"
-
+    for _, pdata in game.players.items():
+        الاعب = pdata["name"]
+        نقاطة = pdata["points"]
+        msg+= f'{الاعب=}  -  {نقاطة=}\n'
     await e.reply(msg)
-
-
 @ABH.on(events.NewMessage(pattern='تم', incoming=True))
 async def start_game(e):
     chat = e.chat_id
-
-    if chat not in killamordersession:
-        return
-
+    game = killamorder.get(chat)
+    if not game:return
+    if e.sender_id != game.owner:
+        return await e.reply("أنت لست مالك اللعبة")
     await e.reply("يتم بدء اللعبة ...")
-    players_count = len(killamordersession[chat]["players"]) * 2
-
+    players_count = len(game.players) * 2
     for _ in range(players_count):
         await set_auto_killer(e)
-        await asyncio.sleep(10)
-
-
+        await asyncio.sleep(random.randint(6, 12))
 async def set_auto_killer(e):
     chat = e.chat_id
-    session = killamordersession[chat]
-    players = session["players"]
-
-    # لا يوجد لاعبين
+    game = killamorder[chat]
+    players = game.players
     if not players:
-        del killamordersession[chat]
-        return
-
-    # فائز وحيد
+        game.end()
     if len(players) == 1:
-        winner = next(iter(players.values()))["name"]
-        await e.reply(f"🎉 مبروك! الفائز هو: {winner}")
-        del killamordersession[chat]
+        winner = game.player.keys()[0]
+        اسمه = winner['name']
+        محاولاته = winner['points']
+        await e.reply(f"🎉 مبارك للاعب ( {اسمه=} ) - محاولاته المتبقية: {محاولاته}")
+        game.end()
         return
-
-    # اختيار القاتل
     player_id, pdata = random.choice(list(players.items()))
     killer_name = pdata["name"]
     points = pdata["points"]
-
-    # ان مات (0 نقاط)
     if points <= 0:
         await e.reply(f"الله يرحمك ( {killer_name} ) — ماتت نقاطك")
-        del players[player_id]
+        game.remove_player(player_id)
         return
-
-    # حفظ القاتل
-    session["killer"] = player_id
-
-    # أزرار
+    game.killer = player_id
     btns = [
         Button.inline("تحديد الضحية", data="choice_to_kill"),
         Button.inline("قتل عشوائي", data="autokill")
     ]
-
     await e.reply(f"أنت القاتل يا ( {killer_name} ) — اختر نوع القتل", buttons=btns)
-
-    # إنقاص النقاط
-    session["players"][player_id]["points"] -= 1
-
-
-@ABH.on(events.CallbackQuery)
+    game.players[player_id]["points"] -= 1
+@ABH.on(events.CallbackQuery(pattern=r"choice_to_kill|autokill|kill:(\d+)"))
 async def kill_callback(e):
     chat = e.chat_id
-    session = killamordersession.get(chat)
-    if not session:
+    game = killamorder.get(chat)
+    if not game:
+        await e.delete()
         return
-
-    killer = session.get("killer")
+    killer = game.killer()
     if not killer or e.sender_id != killer:
         return
-
     data = e.data.decode()
-
-    players = session["players"]
-
-    if data == "autokill":  # قتل عشوائي
+    players = game.players()
+    if data == "autokill":
         victim_id, victim_data = random.choice(list(players.items()))
-
         if victim_id == killer:
+            game.del_killer()
             await e.edit(f"انتحر اللاعب ( {victim_data['name']} ) 🤦‍♂️")
-            del players[victim_id]
-            session["killer"] = None
+            game.remove_player(victim_id)
             return
-
+        game.del_killer()
         await e.edit(f"تم قتل اللاعب ( {victim_data['name']} )")
-        del players[victim_id]
-        session["killer"] = None
+        game.remove_player(victim_id)
         return
-    if data == "choice_to_kill":  # تحديد ضحية
+    elif data == "choice_to_kill":
         txt = "اختر الضحية:\n"
         btns = []
-
         for uid, pdata in players.items():
             if uid != killer:
                 btns.append([Button.inline(pdata["name"], data=f"kill:{uid}")])
         await e.edit(txt, buttons=btns)
         return
     if data.startswith("kill:"):
+        game.del_killer()
         victim_id = int(data.split(":")[1])
         victim = players[victim_id]["name"]
-        del players[victim_id]
+        game.remove_player(victim_id)
         await e.edit(f"تم قتل ( {victim} ) بنجاح")
-        session["killer"] = None
+        return
