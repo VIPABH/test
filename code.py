e0776184349979c6220a0556ad3dc2898fafdfec
VIPABH <a891983.2@gmail.com>
2026-07-15 23:19:39 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,8 @@ from telethon import events
 from telethon.tl.types import (
     ReplyInlineMarkup,
     KeyboardButtonRow,
-    KeyboardButtonUserProfile,
+    InputKeyboardButtonUserProfile,
+    InputUser,
 )
 from ABH import ABH
 
@@ -11,13 +12,17 @@ async def main(e):
     target_user_id = 1910015590
     button_text = 'ابـ،ـن،هـ.ـاشـ.ـم ✘'
 
+    # لازم نجيب access_hash الخاص باليوزر عن طريق get_input_entity
+    input_peer = await ABH.get_input_entity(target_user_id)
+    user_input = InputUser(user_id=input_peer.user_id, access_hash=input_peer.access_hash)
+
     markup = ReplyInlineMarkup(
         rows=[
             KeyboardButtonRow(
                 buttons=[
-                    KeyboardButtonUserProfile(
+                    InputKeyboardButtonUserProfile(
                         text=button_text,
-                        user_id=target_user_id
+                        user_id=user_input
                     )
                 ]
             )
