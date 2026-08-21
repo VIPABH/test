5bbc6fbe8842f5015b0787f449458bf5d4e1a170
VIPABH <a891983.2@gmail.com>
2026-08-21 11:47:54 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,51 +1,93 @@
 from Resources import *
 from ABH import *
 import uuid
+import re
+
 whisper_session = {}
+
+
 @ABH.on(events.NewMessage(pattern=r'^(اهمس|همس[هة])(?:\s+(.+))?$'))
 async def whisper(e):
-    # if not lock(e, 'همسة'):
-    #     return await e.reply('اوامر الهمسة معطلة💔')
+
     id = e.sender_id
-    # if id in whisper_session:
-    # target = await to(e)
-    # target_id = getattr(target, "sender_id", None) or getattr(target, "id", None)
-    # if not target:return await e.reply("حاول تشغل الامر اما بالرد او باليوزر او المنشن")
-    # if e.reply_to_msg_id:
-    #     r = await e.get_reply_message()
-    # if getattr(target, "bot", False):
-    #     return 
-    # if target_id == id:
-    #     return await e.reply("شني خالي تسوي همسه لنفسك")
+
     anymous = await bot()
-    # if target_id == anymous.id:
-    #     return await e.reply("تسويلي همسه 😁؟")
+
     users = []
     targets = e.pattern_match.group(2)
-    if not targets:return await react(e, '😁')
+
+    if not targets:
+        return await react(e, '😁')
+
     async def custom_user(user):
+
+        user = user.strip()
+
+        if not user:
+            return
+
+        # ID
         if user.isdigit():
-            users.append(user)
-        elif user.startswith('@'):
-            full_user = await ABH.get_entity(user)
-            users.append(full_user.id)
-    if len(targets) > 1:
-        for user in targets:
-            await custom_user(user)
-    else:
-        await custom_user(users)
+            users.append(int(user))
+
+        # Username
+        elif user.startswith('@') and len(user) > 1:
+            try:
+                full_user = await ABH.get_entity(user)
+                users.append(full_user.id)
+            except ValueError:
+                return
+
+    # استخراج اليوزرات والـ IDs
+    for user in re.findall(r'@\w+|\d+', targets):
+        await custom_user(user)
+
+    if not users:
+        return await e.reply(
+            "ما لكيت مستخدم صالح."
+        )
+
     owner_name = await mention(e)
+
     whisper_id = str(uuid.uuid4())[:6]
-    whisper_session.setdefault(id, {})
+
     whisper_session[id] = {
         'owmer': e.sender_id,
         'owner_name': owner_name,
         'to': users,
-        'to_name': [await ment(user) for user in users],
-        'whisper_id': str(uuid.uuid4())[:6],
-        }
+        'to_name': [
+            await ment(user)
+            for user in users
+        ],
+        'whisper_id': whisper_id,
+    }
+
     url = f"https://t.me/{anymous.username}?start={whisper_id}"
-    start_button = Button.url('اضغط هنا للبدء', url=url, style=blue)
-    del_button = Button.inline("حذف الهمسة", data=f'del_l:{id}', style=red)
-    text = f'همسة جارية الانشاء من ( {await mention(e)} ) إلى ( {'و'.join(whisper_session[id]['to_name'])} ) 🙂🙂'
-    await e.reply(text, buttons=[start_button])
+
+    start_button = Button.url(
+        'اضغط هنا للبدء',
+        url=url
+    )
+
+    del_button = Button.inline(
+        "حذف الهمسة",
+        data=f'del_l:{id}'
+    )
+
+    to_names = ' و '.join(
+        whisper_session[id]['to_name']
+    )
+
+    text = (
+        f'همسة جارية الانشاء من '
+        f'( {owner_name} ) إلى '
+        f'( {to_names} ) 🙂🙂'
+    )
+
+    await e.reply(
+        text,
+        buttons=[
+            [start_button],
+            [del_button]
+        ]
+    )
