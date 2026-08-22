01a9a41a71392d6d8049832a5281ae28313f1c4a
VIPABH <a891983.2@gmail.com>
2026-08-22 13:35:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -51,7 +51,7 @@ async def whisper(e):
         'msg': msg.id,
         }
 @ABH.on(events.NewMessage(pattern=r'/start (\w+)'))
-async def start_with_param(event):
+async def start_with_param(e):
     whisper_id = event.pattern_match.group(1)
     id = e.sender_id
     if not id in whisper_session[id]:
