f03600f7b7a1d25ab3b69e46b2d6fec8433813e4
VIPABH <a891983.2@gmail.com>
2026-08-22 13:34:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,24 +1,83 @@
-import asyncio
-import re
-import uuid
-from ABH import ABH as client
 from Resources import *
-from telethon import events
-
-
-@client.on(events.NewMessage(incoming=True))
-async def handle_media(event):
-  # التحقق مما إذا كانت الرسالة تحتوي على ميديا (صورة، فيديو، إلخ)
-  if not event.media:
-    return
-
-  e = event
-  await e.reply(str(event))
-  await e.reply(event.grouped_id)
-  if event.grouped_id:
-    await event.reply(
-        f"تم استلام عنصر من ألبوم (Group ID: {event.grouped_id}) - رقم الرسالة:"
-        f" {event.id}"
-    )
-  else:
-    await event.reply(f"تم استلام ميديا مفردة برقم الرسالة: {event.id}")
+from ABH import *
+import uuid, re
+whisper_session = {}
+@ABH.on(events.NewMessage(pattern=r'^(اهمس|همس[هة])(?:\s+(.+))?$'))
+async def whisper(e):
+    id = e.sender_id
+    anymous = await bot()
+    users = set()
+    targets = e.pattern_match.group(2)
+    if not targets:
+        return await react(e, '😁')
+    if id in whisper_session:
+        session = whisper_session[id]
+        text = f'عذرا ماتكدر تسوي همسة \n عندك جلسة بعدك ما مكملها'
+        del_button = [
+            Button.inline("حذف الهمسة",data=f'del_l:{id}', style=red, icon=5258130763148172425),
+            Button.url("أكمال الهمسة",url=f"https://t.me/{anymous.username}?start={session['whisper_id']}", style=green, icon=5258073068852485953),
+            Button.url("رابط الهمسة",url=session['link'], style=blue, icon=5258262708838472996),]
+        button = chunk_list(del_button, 2)
+        return await e.reply(text, buttons=button)
+    async def custom_user(user):
+        user = user.strip()
+        if not user:return
+        try:
+            full_user = await ABH.get_entity(user)
+            if not getattr(full_user, "bot", False):
+                users.add(full_user.id)
+        except ValueError:
+            return
+    for user in re.findall(r'@\w+|\d+', targets):
+        await custom_user(user)
+    users = list(users)
+    if not users:return await e.reply("ما لكيت المستخدم.")
+    owner_name = await mention(e)
+    whisper_id = str(uuid.uuid4())[:6]
+    url = f"https://t.me/{anymous.username}?start={whisper_id}"
+    start_button = Button.url('اضغط هنا للبدء',url=url, style=green, icon=5258073068852485953)
+    _mentions = [await ment(user) for user in users]
+    to_names = ' و '.join(_mentions)
+    text = (
+        f'همسة جارية الانشاء من '
+        f'( {owner_name} ) إلى '
+        f'( {to_names} ) 🙂🙂')
+    msg = await e.reply(text, buttons=[start_button])
+    whisper_session[id] = {
+        'to': users,
+        'to_name': _mentions,
+        'whisper_id': whisper_id,
+        'link': row_link(e),
+        'msg': msg.id,
+        }
+@ABH.on(events.NewMessage(pattern=r'/start (\w+)'))
+async def start_with_param(event):
+    whisper_id = event.pattern_match.group(1)
+    id = e.sender_id
+    if not id in whisper_session[id]:
+        return await chs(e, 'عزيزي انت اصلا ما عندك جلسة اهمس')
+    session = whisper_session[id]
+    whisper_id = session['whisper_id']
+    await chs(e, 'ارسل الان همسة ميديا او نص')
+@ABH.on(events.NewMessage(incoming=True, from_users=list(map(int, whisper_session.keys()))))
+async def recive_whisper(e):
+    if not e.is_private:return
+    if e.text.startswith('/start'):return
+    msg = event.message
+    id = e.sender_id
+    media = {}
+    if msg.media:
+        if e.grouped_id:
+            if id not in media: await chs(e, 'تم ارسال الهمسة ب نجاح')
+            media[id] = {e.grouped_id: set()}
+            media[id].add(await extract_media_data(e))
+        else:
+            media[id] = await extract_media_data(e)
+@ABH.on(events.CallbackQuery(pattern=b'^del_l:(\\d+)$'))
+async def delete_whisper_callback(e):
+    data = e.data.decode('utf-8')
+    id = int(data.replace('del_l:', ''))
+    sender_id = e.sender_id
+    if id != sender_id:
+        return await e.answer('🙄')
+    await e.edit('تم حذف جلسة الهمسة', buttons=Button.url("كيف اهمس", url=f"https://t.me/{(await bot()).username}?start=how_can_i_whisper"))
