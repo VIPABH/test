ba8eafdb4d0f7ba15f95a51f60cf83bc349b23d9
VIPABH <a891983.2@gmail.com>
2026-08-31 12:53:40 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,12 @@
 from ABH import *
 from telethon.tl.custom import Button
+
 @ABH.on(events.NewMessage)
 async def _(e):
-    await e.reply('>', buttons=[Button.mention('--', e.sender_id)])
+    # جلب الـ InputEntity الخاص بالمرسل لتمريره لـ Button.mention
+    user_entity = await ABH.get_input_entity(e.sender_id)
+    
+    # إرسال الرسالة مع زر المنشن
+    await e.reply('>', buttons=[
+        [Button.mention('--', user_entity)]
+    ])
