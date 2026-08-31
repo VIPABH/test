48ca0b08d810bd550093e8899893d8fc94a338e2
VIPABH <a891983.2@gmail.com>
2026-08-31 12:55:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,25 @@
-from ABH import *
-from telethon import events
+from telethon import events, types
 from telethon.tl.custom import Button
-from telethon.tl import types
+from ABH import *
 
-# تعريف دالة mention وإضافتها لكلاس Button
-def mention_button(text, user):
-    return types.InputKeyboardButtonUserProfile(text=text, user_id=user)
+def mention_button(text, user_entity):
+    # تحويل الـ InputEntity إلى InputUser يتضمن access_hash
+    input_user = types.InputUser(
+        user_id=user_entity.user_id,
+        access_hash=user_entity.access_hash
+    )
+    return types.KeyboardButtonRow(
+        buttons=[types.InputKeyboardButtonUserProfile(text=text, user_id=input_user)]
+    )
 
 Button.mention = staticmethod(mention_button)
 
-
 @ABH.on(events.NewMessage)
 async def _(e):
+    # جلب entity المستخدم المرسل
     user_entity = await ABH.get_input_entity(e.sender_id)
     
+    # إرسال الزر المغلف
     await e.reply('>', buttons=[
         [Button.mention('--', user_entity)]
     ])
