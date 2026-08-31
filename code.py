6e3c2faa21949bbac1990a3e83bf23a52446b6b8
VIPABH <a891983.2@gmail.com>
2026-08-31 13:08:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,8 +8,9 @@ from telethon.tl.types import (
 from ABH import ABH
 @ABH.on(events.NewMessage(pattern=r'\.زر'))
 async def button_mention(e, id=None, text=None):
-    if not id:return
-    input_peer = await ABH.get_input_entity(id)    
+    # if not id:return
+    
+    input_peer = await ABH.get_input_entity(e.sender_id)    
     button_text = text if text else getattr(user_entity, 'first_name', 'User')    
     user_input = InputUser(user_id=input_peer.user_id, access_hash=input_peer.access_hash)    
     markup = ReplyInlineMarkup(
